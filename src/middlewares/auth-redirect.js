f219aaf93141580adcbd2ee3b0bd788d5f1448bc
Ed Brett <e.j.a.brett@gmail.com>
2017-05-13 13:26:48 +0200

    update pkjson to fix dependancies, change return url

--- src/middlewares/auth-redirect.js
+++ src/middlewares/auth-redirect.js
@@ -2,12 +2,7 @@ import { replace } from 'react-router-redux';
 import { publicRoutes } from '../constants/routes';
 
 const authRedirectMiddleware = store => next => (action) => {
-  // const { loggedIn } = store.getState().user;
-  // if (!loggedIn && !publicRoutes.includes(action.payload.pathname)) {
-  // } else {
-    next(action);
-  // }
-
+  next(action);
 };
 
 export default authRedirectMiddleware;
