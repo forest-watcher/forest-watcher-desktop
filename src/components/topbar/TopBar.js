9ff5e7755c7ee8b802159fbbb0aefb91eb7f7aab
Ed Brett <e.j.a.brett@gmail.com>
2017-05-13 17:39:16 +0200

    conditional for logut only to logged in users

--- src/components/topbar/TopBar.js
+++ src/components/topbar/TopBar.js
@@ -3,11 +3,19 @@ import React from 'react';
 class TopBar extends React.Component {
   render() {
     console.log(this.props);
-    return (
+    if (this.props.user.loggedIn) return (
       <header className="l-top-bar" role="banner">
         <nav className="c-nav">
           <li>Forest Watcher 2.0</li>
           <li onClick={this.props.logout}>Logout</li>
+        </nav>
+      </header>
+    );
+
+    return (
+      <header className="l-top-bar" role="banner">
+        <nav className="c-nav">
+          <li>Forest Watcher 2.0</li>
         </nav>
       </header>
     );
