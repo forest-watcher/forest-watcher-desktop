854a5c4317f7cb5136a23cf26f584b87df479816
sorodrigo <rodrigo.solis@vizzuality.com>
2017-05-22 20:49:47 +0200

    wip: nav links styles

--- src/components/layouts/nav/Nav.js
+++ src/components/layouts/nav/Nav.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
 
 function Nav({ user, logout }) {
   return (
     <nav className="c-nav">
-      <li className="nav-home">Forest Watcher 2.0</li>
-      {user.loggedIn && <li onClick={logout}>Logout</li>}
+      <ul className="nav-links">
+        <li className="nav-link -secondary">
+          <NavLink exact to="/">Forest Watcher 2.0</NavLink>
+        </li>
+      </ul>
+      {user.loggedIn &&
+        <ul className="nav-links">
+          <li className="nav-link">
+            <NavLink to="/areas">Areas</NavLink>
+          </li>
+          <li className="nav-link">
+            <NavLink to="/areas">Reports</NavLink>
+          </li>
+        </ul>
+      }
+      {user.loggedIn &&
+        <ul>
+          <li className="nav-link -secondary">
+            <a onClick={logout}>Log out</a>
+          </li>
+        </ul>
+      }
     </nav>
   );
 }
