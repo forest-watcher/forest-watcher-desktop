2a508a41a56e335cb2e66e753210bc8347a8e0e2
sorodrigo <rodrigo.solis@vizzuality.com>
2017-05-25 19:05:41 +0200

    feat: includes created at in report-card + styles card fields

--- src/components/report-card/ReportCard.js
+++ src/components/report-card/ReportCard.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import fecha from 'fecha';
+
 import Card from '../ui/Card';
 
 class ReportCard extends React.Component {
+
+  getFormatedDate = (date) => {
+    return fecha.format(new Date(date), 'DMMM HH:mm').toUpperCase();
+  }
 
   render() {
     const { report } = this.props;
     return (
       <Card
         title={report.name}
-        actions={[{ iconName: 'icon-info', color: '-green', callback: () => console.info('info report') }]}
+        fields={[this.getFormatedDate(report.createdAt)]}
+        actions={[{
+          iconName: 'icon-info',
+          color: '-green',
+          callback: () => console.info('info report')
+        }]}
       />
     );
   }
