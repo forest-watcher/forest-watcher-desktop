8080770f58b8b9bbd5c6cf74b6ebb1ca2b649a00
Edward Brett <e.j.a.brett@gmail.com>
2017-06-01 17:22:27 +0200

    conditional tools for creating, editing, and deleting map polygon

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -8,61 +8,81 @@ const BASEMAP_TILE = 'http://server.arcgisonline.com/ArcGIS/rest/services/World_
 const MAP_CENTER = [51.505, -0.09];
 const BASEMAP_ATTRIBUTION = 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
 
+const editableLayers = new L.FeatureGroup();
+const drawControlFull = {
+  position: 'topright',
+  draw: {
+      polyline: false,
+      polygon: {
+          allowIntersection: false, // Restricts shapes to simple polygons
+          drawError: {
+              color: '#e1e100', // Color the shape will turn when intersects
+              message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
+          },
+          shapeOptions: {
+              color: '#bada55'
+          }
+      },
+      circle: false,
+      rectangle: {
+          shapeOptions: {
+              clickable: false
+          }
+      },
+      marker: false
+  },
+  edit: {
+      featureGroup: editableLayers, //REQUIRED
+      remove: true
+  }
+};
+const drawControlEdit = {
+  position: 'topright',
+  draw: {
+      polyline: false,
+      polygon: false,
+      circle: false,
+      rectangle: false,
+      marker: false
+  },
+  edit: {
+      featureGroup: editableLayers, //REQUIRED
+      remove: true
+  }
+};
+
 class Map extends React.Component {
 
   componentDidMount() {
     this.initMap();
+    this.initLayers();
+    this.initDrawing();
   }
 
   componentWillUnmount() {
     this.remove();
   }
 
-  enableDrawing() {
-    const editableLayers = new L.FeatureGroup();
-    this.map.addLayer(editableLayers);
+  onDrawEventComplete(e) {
+    const layer = e.layer;
 
-    var options = {
-      position: 'topright',
-      draw: {
-          polyline: false,
-          polygon: {
-              allowIntersection: false, // Restricts shapes to simple polygons
-              drawError: {
-                  color: '#e1e100', // Color the shape will turn when intersects
-                  message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
-              },
-              shapeOptions: {
-                  color: '#bada55'
-              }
-          },
-          circle: false, // Turns off this drawing tool
-          rectangle: {
-              shapeOptions: {
-                  clickable: false
-              }
-          },
-          marker: false
-      },
-      edit: {
-          featureGroup: editableLayers, //REQUIRED!!
-          remove: true
-      }
-    };
+    editableLayers.addLayer(layer);
 
-    const drawControl = new L.Control.Draw(options);
-    this.map.addControl(drawControl);
+    if (Object.keys(editableLayers._layers).length > 0) {
+      this.drawControlFull.remove(this.map);
+      this.drawControlEdit.addTo(this.map);
+    }
+  }
 
-    this.map.on(L.Draw.Event.CREATED, function (e) {
-      var type = e.layerType,
-          layer = e.layer;
+  onDrawEventDelete(e) {
+    const layer = e.layer;
 
-      if (type === 'marker') {
-          layer.bindPopup('A popup!');
-      }
+    editableLayers.removeLayer(layer);
 
-      editableLayers.addLayer(layer);
-    });
+    if (Object.keys(editableLayers._layers).length < 1) {
+      this.drawControlEdit.remove(this.map);
+      this.drawControlFull.addTo(this.map);
+    }
   }
 
   initMap() {
@@ -77,8 +97,25 @@ class Map extends React.Component {
     this.map.zoomControl.setPosition('topright');
     this.map.scrollWheelZoom.disable();
     this.tileLayer = L.tileLayer(BASEMAP_TILE).addTo(this.map).setZIndex(0);
+  }
 
-    this.enableDrawing();
+  initLayers() {
+    this.map.addLayer(editableLayers);
+  }
+
+  initDrawing() {
+    this.drawControlFull = new L.Control.Draw(drawControlFull);
+    this.drawControlEdit = new L.Control.Draw(drawControlEdit);
+
+    this.map.addControl(this.drawControlFull);
+
+    this.map.on(L.Draw.Event.CREATED, (e) => {
+      this.onDrawEventComplete(e);
+    });
+
+    this.map.on(L.Draw.Event.DELETED, (e) => {
+      this.onDrawEventDelete(e);
+    });
   }
 
   remove() {
