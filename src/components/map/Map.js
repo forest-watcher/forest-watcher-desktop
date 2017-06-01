1e9fa6d2dbf500dcd2123b4e3844859c6f6d6cc6
Edward Brett <e.j.a.brett@gmail.com>
2017-06-01 15:50:11 +0200

    add polygon and rectangle drawing to map

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import L from 'leaflet';
+import { Draw, Control } from 'leaflet-draw';
 
 const MAP_MIN_ZOOM = 2;
 const MAP_INITIAL_ZOOM = 1;
@@ -17,6 +18,53 @@ class Map extends React.Component {
     this.remove();
   }
 
+  enableDrawing() {
+    const editableLayers = new L.FeatureGroup();
+    this.map.addLayer(editableLayers);
+
+    var options = {
+      position: 'topright',
+      draw: {
+          polyline: false,
+          polygon: {
+              allowIntersection: false, // Restricts shapes to simple polygons
+              drawError: {
+                  color: '#e1e100', // Color the shape will turn when intersects
+                  message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
+              },
+              shapeOptions: {
+                  color: '#bada55'
+              }
+          },
+          circle: false, // Turns off this drawing tool
+          rectangle: {
+              shapeOptions: {
+                  clickable: false
+              }
+          },
+          marker: false
+      },
+      edit: {
+          featureGroup: editableLayers, //REQUIRED!!
+          remove: true
+      }
+    };
+
+    const drawControl = new L.Control.Draw(options);
+    this.map.addControl(drawControl);
+
+    this.map.on(L.Draw.Event.CREATED, function (e) {
+      var type = e.layerType,
+          layer = e.layer;
+
+      if (type === 'marker') {
+          layer.bindPopup('A popup!');
+      }
+
+      editableLayers.addLayer(layer);
+    });
+  }
+
   initMap() {
     this.map = L.map('map', {
       minZoom: MAP_MIN_ZOOM,
@@ -29,6 +77,8 @@ class Map extends React.Component {
     this.map.zoomControl.setPosition('topright');
     this.map.scrollWheelZoom.disable();
     this.tileLayer = L.tileLayer(BASEMAP_TILE).addTo(this.map).setZIndex(0);
+
+    this.enableDrawing();
   }
 
   remove() {
