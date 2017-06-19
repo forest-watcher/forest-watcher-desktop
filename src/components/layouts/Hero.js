f0fa28d96b85dde876d6c9323f1d948e9922ad09
Edward Brett <e.j.a.brett@gmail.com>
2017-06-19 10:32:17 +0200

    fix: update css to fit to grid correctly and stack padding vertically

--- src/components/layouts/Hero.js
+++ src/components/layouts/Hero.js
@@ -4,12 +4,10 @@ import PropTypes from 'prop-types';
 function Hero({ title, action }) {
   return (
     <aside className="c-hero">
-      <div className="row">
-        <div className="column small-12">
-          <div className="hero-content">
-            <h1 className="text -large-title -white">{title}</h1>
-            {action && <button onClick={action.callback}>{action.name}</button>}
-          </div>
+      <div className="row column">
+        <div className="hero-content">
+          <h1 className="text -large-title -white">{title}</h1>
+          {action && <button onClick={action.callback}>{action.name}</button>}
         </div>
       </div>
     </aside>
--- src/components/layouts/Nav.js
+++ src/components/layouts/Nav.js
@@ -5,36 +5,34 @@ import Icon from '../ui/Icon';
 
 function Nav({ loggedIn, logout }) {
   return (
-    <div className="row">
-      <div className="column small-12">
-        <nav className="c-nav">
-          <h1 className="nav-logo">
-            <NavLink exact to="/" activeClassName="-active">
-              <Icon className="-small" name="icon-forest-watcher-small"/>
-              Forest Watcher 2.0
-            </NavLink>
-          </h1>
-          <div className="nav-section">
-            {loggedIn &&
-              <ul className="nav-subsection">
-                <li className="nav-link">
-                  <NavLink to="/areas" activeClassName="-active">Areas</NavLink>
-                </li>
-                <li className="nav-link">
-                  <NavLink to="/reports" activeClassName="-active">Reports</NavLink>
-                </li>
-              </ul>
-            }
-            {loggedIn &&
-              <ul>
-                <li className="nav-menu">
-                  <a onClick={logout}>Log out</a>
-                </li>
-              </ul>
-            }
-          </div>
-        </nav>
-      </div>
+    <div className="row column">
+      <nav className="c-nav">
+        <h1 className="nav-logo">
+          <NavLink exact to="/" activeClassName="-active">
+            <Icon className="-small" name="icon-forest-watcher-small"/>
+            Forest Watcher 2.0
+          </NavLink>
+        </h1>
+        <div className="nav-section">
+          {loggedIn &&
+            <ul className="nav-subsection">
+              <li className="nav-link">
+                <NavLink to="/areas" activeClassName="-active">Areas</NavLink>
+              </li>
+              <li className="nav-link">
+                <NavLink to="/reports" activeClassName="-active">Reports</NavLink>
+              </li>
+            </ul>
+          }
+          {loggedIn &&
+            <ul>
+              <li className="nav-menu">
+                <a onClick={logout}>Log out</a>
+              </li>
+            </ul>
+          }
+        </div>
+      </nav>
     </div>
   );
 }
