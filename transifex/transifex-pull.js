c241c1b0c0f061375f4fdac2e917399c0bff7f3c
Edward Brett <e.j.a.brett@gmail.com>
2017-06-30 17:14:37 +0200

    tidy up scripts

--- transifex/transifex-pull.js
+++ transifex/transifex-pull.js
@@ -1,12 +1,15 @@
-// Pulls all current translations for all file
-
+// Script to request all active languages on transifex and then fetch their respective translations
 var fetch = require('node-fetch');
-require('dotenv').config();
 var fs = require('fs');
+require('dotenv').config();
 
-const languagesUrl = `${process.env.TRANSIFEX_URL}/${process.env.TRANSIFEX_PROJECT}/resource/${process.env.TRANSIFEX_SLUG}/?details`;
-const translationsUrl = `${process.env.TRANSIFEX_URL}/${process.env.TRANSIFEX_PROJECT}/resource/${process.env.TRANSIFEX_SLUG}/translation`;
+// Endpoint for fetching available languages
+const langUrl = `${process.env.TRANSIFEX_URL}/${process.env.TRANSIFEX_PROJECT}/resource/${process.env.TRANSIFEX_SLUG}/?details`;
 
+// Endpoint for fetching translations
+const transUrl = `${process.env.TRANSIFEX_URL}/${process.env.TRANSIFEX_PROJECT}/resource/${process.env.TRANSIFEX_SLUG}/translation`;
+
+// Fetch config
 const fetchConfig = {
     headers: {
         Authorization: `Basic ${process.env.TRANSIFEX_API_TOKEN}`
@@ -14,29 +17,27 @@ const fetchConfig = {
     method: 'GET'
 }
 
+// Lets start
 console.log('Fetching available languages...');
-fetch(languagesUrl, fetchConfig)
+
+fetch(langUrl, fetchConfig)
     .then(function(res) {
         console.log('Languages found!');        
         return res.json();
     })
     .then(function(json) {
-        const languageKeys = json.available_languages.map((language) => {
-            const lang = language.code;
-            return lang;
+        const languages = json.available_languages.map((language) => {
+            return language.code;
         });
-        return languageKeys;
+        return languages;
     })
-    .then(function(languageKeys) {
-        languageKeys.forEach((lang) => {
-            console.log(`Fetching translations for ${lang}`);    
-            fetch(`${translationsUrl}/${lang}`, fetchConfig)
+    .then(function(languages) {
+        languages.forEach((lang) => { 
+            fetch(`${transUrl}/${lang}`, fetchConfig)
                 .then(function(res) {
-                    console.log(`${lang} translations found...`);
                     return res.json();
                 })
                 .then(function(json) {
-                    console.log(`Saving to file ${lang}.json in ${process.env.LOCALES_PATH}`);
                     fs.writeFile(`${process.env.LOCALES_PATH}/${lang}.json`, JSON.stringify(JSON.parse(json.content), null, 4), (err) => {
                         if (err) throw err;
                         console.log(`Translations for ${lang} successfully saved!`);
@@ -44,4 +45,7 @@ fetch(languagesUrl, fetchConfig)
                 });
         });
 
+    })
+    .catch((err) => {
+        console.log(err);
     });
