69388e09c2328c55edc9aa3e6166cfe0f346bb88
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-07-03 11:55:32 +0200

    Fix checkbox

--- src/components/ui/Checkbox.js
+++ src/components/ui/Checkbox.js
@@ -4,13 +4,11 @@ import PropTypes from 'prop-types';
 function Checkbox({ id, callback, label, defaultChecked}) {
   return (
     <div className="c-checkbox">
-      <div className="checkbox-container">
-        <div className="checkbox">
-          <input type="checkbox" id={id} onChange={callback} defaultChecked={defaultChecked}/>
-          <label htmlFor={id}></label>
-        </div>
-        <div className="label">{label}</div>
+      <div className="checkbox">
+        <input type="checkbox" id={id} onChange={callback} defaultChecked={defaultChecked}/>
+        <label htmlFor={id}></label>
       </div>
+      <div className="label">{label}</div>
     </div>
   );
 }
