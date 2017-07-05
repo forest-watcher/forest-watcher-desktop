02b2f3a7ebb820fa2810c078f4d991c394b59a10
Edward Brett <e.j.a.brett@gmail.com>
2017-07-05 10:53:57 +0200

    remove className from component

--- src/components/ui/LocateUser.js
+++ src/components/ui/LocateUser.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classnames from 'classnames';
 import Icon from './Icon';
 
 export default class LocateUser extends React.Component {
