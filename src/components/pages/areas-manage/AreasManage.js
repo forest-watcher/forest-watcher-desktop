50706cc8a40b97fcf538dbfa1398beafb86d93a7
Edward Brett <e.j.a.brett@gmail.com>
2017-07-06 19:20:41 +0200

    remove locate user when editing area, as not needed

--- src/components/pages/areas-manage/AreasManage.js
+++ src/components/pages/areas-manage/AreasManage.js
@@ -107,18 +107,20 @@ class AreasManage extends React.Component {
               }}
             />
             <div className="c-map-controls">
-              <LocateUser 
-                map={this.state.map} 
-                setLoading={this.props.setLoading}
-                onZoomChange={ (zoom) => {
-                  this.setState({
-                    mapConfig: {
-                      ...this.state.mapConfig,
-                      zoom
-                    }
-                  });
-                }}
-              />
+              { this.props.mode === 'create' &&
+                <LocateUser 
+                  map={this.state.map} 
+                  setLoading={this.props.setLoading}
+                  onZoomChange={ (zoom) => {
+                    this.setState({
+                      mapConfig: {
+                        ...this.state.mapConfig,
+                        zoom
+                      }
+                    });
+                  }}
+                />
+              }
               <ZoomControl
                 map={this.state.map}
                 zoom={this.state.mapConfig.zoom}
