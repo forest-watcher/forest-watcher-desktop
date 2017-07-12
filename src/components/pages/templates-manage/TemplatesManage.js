7fc08296eba716eb762e7065c5e92e1239b46552
Edward Brett <e.j.a.brett@gmail.com>
2017-07-12 14:24:41 +0200

    add meta selectors and css wrapper for form

--- src/components/pages/templates-manage/TemplatesManage.js
+++ src/components/pages/templates-manage/TemplatesManage.js
@@ -2,26 +2,76 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Hero from '../../layouts/Hero';
-import Article from '../../layouts/Article';
 import 'react-select/dist/react-select.css';
 import { FormattedMessage } from 'react-intl';
 import { injectIntl } from 'react-intl';
+import { Form } from '../../form/Form';
+import Select from 'react-select';
 
 class TemplatesManage extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      selectedArea: this.props.selectedArea || '',
+      defaultLanguage: this.props.selectedArea || ''
+    }
+  }
+
+  onAreaChange = (selected) => {
+    this.setState({selectedArea: selected});
+  }
+
+  onLanguageChange = (selected) => {
+    this.setState({defaultLanguage: selected});
+  }
 
   render() {
-    const { templates } = this.props;
+    const { areasOptions, languageOptions } = this.props;
     return (
       <div>
         <Hero
-          title="Create a template"
+          title="templates.create"
         />
-          <div className="l-content">
-            <Article>
-              <div className="l-loader">
+        <div className="l-template">
+          <div className="template-meta">
+            <Form onSubmit={this.handleSubmit}>
+              <div className="c-form">
+                <div className="row -main">
+                  <div className="column small-12 medium-4 medium-offset-2">
+                    <div className="input-group">
+                      <label className="text"><FormattedMessage id={"templates.assignArea"} />:</label>
+                      <Select
+                        multi
+                        simpleValue
+                        name="areas-select"
+                        options={areasOptions}
+                        value={this.state.selectedArea}
+                        onChange={this.onAreaChange}
+                        noResultsText={this.props.intl.formatMessage({ id: 'filters.noAreasAvailable' })}
+                        searchable={false}
+                      />
+                    </div>
+                  </div>
+                  <div className="column small-12 medium-4">
+                    <div className="input-group">
+                      <label className="text"><FormattedMessage id={"templates.defaultLanguage"} />:</label>
+                      <Select
+                        multi
+                        simpleValue
+                        name="language-select"
+                        options={languageOptions}
+                        value={this.state.defaultLanguage}
+                        onChange={this.onLanguageChange}
+                        noResultsText={this.props.intl.formatMessage({ id: 'filters.noLanguagesAvailable' })}
+                        searchable={false}
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
-            </Article>
+            </Form>
           </div>
+        </div>
       </div>
     );
   }
--- src/components/pages/templates-manage/TemplatesManageContainer.js
+++ src/components/pages/templates-manage/TemplatesManageContainer.js
@@ -2,9 +2,36 @@ import { connect } from 'react-redux';
 
 import TemplatesManage from './TemplatesManage';
 
-const mapStateToProps = (state, { match }) => ({
-  templates: state.templates
-});
+const mapAreasToOptions = (areas) => {
+    const areasOptions = [];
+    const areasIds = areas.ids;
+    areasIds.forEach((id) => {
+        areasOptions.push({
+            option: id,
+            label: areas.data[id].attributes.name 
+        });
+    })
+    return areasOptions;
+};
+
+const mapStateToProps = (state, { match }) => {
+    const areasOptions = mapAreasToOptions(state.areas);
+    const languageOptions = [
+        {
+            option: 'en',
+            label: 'English'
+        },
+        {
+            option: 'es',
+            label: 'Espanol'
+        }
+    ];
+    return {
+        templates: state.templates,
+        areasOptions,
+        languageOptions
+    }
+};
 
 function mapDispatchToProps(dispatch) {
   return {
