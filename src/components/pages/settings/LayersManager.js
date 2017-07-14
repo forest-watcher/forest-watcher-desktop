e155cd6095d0d36068811ba3907840b4207e5e7f
Alvaro Leal <aleal@icometrix.com>
2017-07-14 11:36:21 +0200

    No need for class

--- src/components/pages/settings/LayersManager.js
+++ src/components/pages/settings/LayersManager.js
@@ -3,21 +3,19 @@ import React from 'react';
 import LayersForm from './LayersFormContainer';
 import LayersSwitcher from './LayersSwitcherContainer';
 
-class LayersManager extends React.Component {  
-  render() {
-    return (
-      <div className="c-layers-manager">
-        <div className="row">
-          <div className="small-5 columns">
-            <LayersForm />
-          </div>
-          <div className="small-offset-1 small-6 columns">
-            <LayersSwitcher />
-          </div>
+function LayersManager() {
+  return(
+    <div className="c-layers-manager">
+      <div className="row">
+        <div className="small-5 columns">
+          <LayersForm />
+        </div>
+        <div className="small-offset-1 small-6 columns">
+          <LayersSwitcher />
         </div>
       </div>
-    );
-  }
+    </div>
+  )
 }
 
 export default LayersManager;
