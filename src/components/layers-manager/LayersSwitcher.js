1a5250bc06c9ae908ed88192c2c781f3be2a75cf
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-07-17 17:50:43 +0200

    Display apart teams layers and my layers

--- src/components/layers-manager/LayersSwitcher.js
+++ src/components/layers-manager/LayersSwitcher.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { FormattedMessage } from 'react-intl';
 import SwitchButton from 'react-switch-button';
@@ -8,28 +9,38 @@ class LayersSwitcher extends React.Component {
     this.props.toggleLayer(layer, !layer.attributes.enabled);
   }
 
+
   render() {
+    const { teamLayers, userLayers } = this.props;
+    const renderLayers = (layers) => (
+      layers.map((layer, i) => (
+        <SwitchButton
+          key={i}
+          name={`${layer.attributes.owner.type}-layer-${i}-${layer.attributes.name}`} 
+          labelRight={layer.attributes.name} 
+          onChange={() => this.toggleLayer(layer)}
+          defaultChecked={layer.attributes.enabled}
+        />
+      ))
+    )
+
     return (
-      <div>
+      <div className="c-layers-switcher">
         <h3><FormattedMessage id={"settings.selectedLayers"} /></h3>
-        <div className="section">
-          {this.props.selectedLayers.map((selectedLayer, i) => (
-            <div key={i}>
-              <SwitchButton 
-                name={`selected${i}${selectedLayer.attributes.name}`} 
-                labelRight={selectedLayer.attributes.name} 
-                onChange={() => this.toggleLayer(selectedLayer)}
-                defaultChecked={selectedLayer.attributes.enabled}
-              />
-              {selectedLayer.attributes.owner.type === 'TEAM' && 
-              <span className="team-flag"><FormattedMessage id={"settings.team"} /></span>
-              }
-            </div>
-          ))}
+        <div className="section layers-switchs">
+          <h4><FormattedMessage id={"settings.teamLayers"} /></h4>
+          {renderLayers(teamLayers)}
+          <h4><FormattedMessage id={"settings.myLayers"} /></h4>
+          {renderLayers(userLayers)}
         </div>
       </div> 
     );
   }
 }
+
+LayersSwitcher.propTypes = {
+  userLayers: PropTypes.array.isRequired,
+  teamLayers: PropTypes.array.isRequired
+};
 
 export default LayersSwitcher;
--- src/components/layers-manager/LayersSwitcherContainer.js
+++ src/components/layers-manager/LayersSwitcherContainer.js
@@ -2,11 +2,13 @@ import { connect } from 'react-redux';
 import LayersSwitcher from './LayersSwitcher';
 import { toggleLayer } from '../../modules/layers';
 
-
 const mapStateToProps = ({ layers, teams }) => {
   const selectedLayers = layers.selectedLayerIds.map((id) => layers.selectedLayers[id]);
+  const teamLayers = selectedLayers.filter((selectedLayer) => selectedLayer.attributes && selectedLayer.attributes.owner.type === 'TEAM')
+  const userLayers = selectedLayers.filter((selectedLayer) => selectedLayer.attributes && selectedLayer.attributes.owner.type === 'USER');
     return { 
-      selectedLayers
+      teamLayers,
+      userLayers
     };
   };
 
