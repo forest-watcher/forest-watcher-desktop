c9e7a9faed5229fe0709e8a3080938ea6e820354
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-07-19 13:32:17 +0200

    Modify tab component and add pill-tab style

--- src/components/ui/Tab.js
+++ src/components/ui/Tab.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 
-function Tab({ options, selectedIndex, handleTabIndexChange }) {
+function Tab({ options, selectedIndex, handleTabIndexChange, pill, style }) {
+  const tabClass = pill ? `c-pill-tab ${style}` : `c-tab -right ${style}`;
   return (
-    <div className="c-tab -right">
+    <div className={ tabClass }>
       <div className="nav-tab">
         {options.map((option, i) => (
           <a 
@@ -23,7 +24,9 @@ function Tab({ options, selectedIndex, handleTabIndexChange }) {
 Tab.propTypes = {
   options: PropTypes.array.isRequired,
   selectedIndex: PropTypes.number.isRequired,
-  handleTabIndexChange: PropTypes.func.isRequired
+  handleTabIndexChange: PropTypes.func.isRequired,
+  pill: PropTypes.bool,
+  style: PropTypes.string
 };
 
 export default Tab;
