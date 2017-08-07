b3997470a1c1b014185a0384899f6347c3b25009
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-08-07 17:48:17 +0200

    fix: adds custom validator to url tile layers

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -3,6 +3,20 @@ import Validation from 'react-validation';
 import ReactSelect from 'react-select';
 import { FormattedMessage } from 'react-intl';
 
+Object.assign(Validation.rules, {
+  urlTile: {
+    hint: value => <span className="form-error is-visible">Text is not a URL tile layer</span>,
+    rule: (value) => {
+      if (!value) return false;
+      const parts = value.split('/{z}/{x}/{y}');
+      if (parts.length !== 2) return false;
+      const isUrl = Validation.rules.url.rule(parts[0]);
+      const isImage = ['.png', '.jpg'].indexOf(parts[1]) !== -1;
+      return (isUrl && isImage);
+    }
+  }
+});
+
 function withWrapper(Component) {
   return class FormWrapper extends React.Component {
     render() {
