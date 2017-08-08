4fcb07782f4a1bdcd1d2598be8d87c7c60ca54ab
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-08-08 11:43:51 +0200

    fix: moves urltile validation to validation-rules

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,6 +1,6 @@
-import { required, url } from '../constants/validation-rules';
+import { required, url, urlTile } from '../constants/validation-rules';
 import Validation from 'react-validation';
 
-const validation = Object.assign(Validation.rules, { required, url });
+const validation = Object.assign(Validation.rules, { required, url, urlTile });
 
 export { validation };
