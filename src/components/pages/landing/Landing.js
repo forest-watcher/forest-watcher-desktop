c27b87ff7ee2d182811d2e4cf0a53d67581a6e12
Alvaro Leal <alvaro.leal@vizzuality.com>
2017-08-22 13:22:49 +0200

    Add header and footer to landing page

--- src/components/pages/landing/Landing.js
+++ src/components/pages/landing/Landing.js
@@ -3,9 +3,24 @@ import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
 
 class Landing extends React.Component {
+  componentDidMount(){
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.innerHTML = "window.liveSettings = {picker: '#transifexTranslateElement',api_key: '9eda410a7db74687ba40771c56abd357',detectlang: false,site: 'gfw-watcher'};";
+    document.body.appendChild(script);
+
+    const scriptLoader = document.createElement('script');
+    scriptLoader.type = 'text/javascript';
+    scriptLoader.async = true;
+    scriptLoader.id = 'loader-gfw';
+    scriptLoader.src = 'http://gfw-assets.s3.amazonaws.com/static/gfw-assets.nightly.js';
+    document.body.appendChild(scriptLoader);
+  }
   render() {
     return (
       <div className="c-landing">
+        <div id="headerGfw"></div>
         <div className="row">
           <div className="small-12 medium-6">
             <div className='logo-image'></div>
@@ -39,6 +54,7 @@ class Landing extends React.Component {
             </div>
           </div>
         </div>
+         <div id="footerGfw"></div>
       </div>
     );
   }
