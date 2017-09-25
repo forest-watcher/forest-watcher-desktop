aabdf13ad16300a615ee3afff9ff0f0fa877ed66
Simao Belchior <simaobelchior@gmail.com>
2017-09-25 15:34:30 +0100

    Move code to routes directly

--- src/helpers/analytics.js
+++
@@ -1,9 +0,0 @@
-import ReactGA from 'react-ga';
-
-ReactGA.initialize('UA-48182293-4'); //Unique Google Analytics tracking number
-
-const fireTracking = () => {
-  ReactGA.pageview(window.location.hash);
-}
-
-export { fireTracking };
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,16 @@ import React from 'react';
 import {
   BrowserRouter as Router
 } from 'react-router-dom'
-import fireTracking from 'helpers/analytics';
+
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-48182293-4'); //Unique Google Analytics tracking number
 
 // App
 import App from './components/app/AppContainer';
+
+const fireTracking = () => {
+  ReactGA.pageview(window.location.hash);
+}
 
 const Routes = () => (
   <Router onUpdate={fireTracking}>
