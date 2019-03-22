264d4d0981d94f2337552cf32aad7bef433400f0
Kiri Egington <kiri.egington@3sidedcube.com>
2019-03-22 10:24:03 +0000

    test for shapefile modal

--- src/components/ui/ShapefileInfo.js
+++ src/components/ui/ShapefileInfo.js
@@ -26,20 +26,20 @@ class ShapefileInfo extends PureComponent {
       <div className="c-modal-content">
         <div className="modal-content-inner">
           <h2 className="text -small-title -green">{title}</h2>
-          <p className="text">{maxSize}</p>
-          <p className="text">{formats}</p>
+          <p className="text test-max-size">{maxSize}</p>
+          <p className="text test-formats">{formats}</p>
 
           <ul className="modal-content-steps-content">
             <li className="modal-content-steps-content-item text">
-              <p className="text"><span className="u-bold">{unzippedTitle}</span> {unzipped}</p>
+              <p className="text test-unzipped"><span className="u-bold test-unzipped-title">{unzippedTitle}</span> {unzipped}</p>
             </li>
             <li className="modal-content-steps-content-item text">
-              <p className="text"><span className="u-bold">{zippedTitle}</span> {zipped}</p>
+              <p className="text test-zipped"><span className="u-bold test-zipped-title">{zippedTitle}</span> {zipped}</p>
             </li>
           </ul>
 
           <div className="modal-content-action">
-            <button className="c-button" onClick={this.onAccept}>ok</button>
+            <button className="c-button test-confirm-button" onClick={this.onAccept}>ok</button>
           </div>
         </div>
       </div>
