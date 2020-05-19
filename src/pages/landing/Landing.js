0604b028ad8e86cf1238781fe483ffded212b02b
Ed Brett <e.j.a.brett@gmail.com>
2020-05-19 21:43:52 +0200

    update gfw assets to latest version and tweak implementation of language selector

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -4,13 +4,12 @@ import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router-dom';
 import { GFW_ASSETS_PATH, DOWNLOAD_APK_LINK, DOWNLOAD_APK_VERSION } from '../../constants/landing';
 import SocialFooter from './SocialFooter';
-import Select from 'react-select';
 import Script from 'react-load-script';
-import DropdownIndicator from '../../components/ui/SelectDropdownIndicator'
 import ReactGA from 'react-ga';
 
-class Landing extends React.Component {
+import fwLogo from './fw-logo.png';
 
+class Landing extends React.Component {
   static propTypes = {
     locale: PropTypes.string,
     setLocale: PropTypes.func,
@@ -19,63 +18,36 @@ class Landing extends React.Component {
 
   constructor(props) {
     super(props);
-    this.handleLanguageChange = this.handleLanguageChange.bind(this);
     this.languages = props.translations && Object.keys(props.translations).map((lang) => (
       { value: lang, label: lang.toUpperCase() }
     ));
     this.state = {
-      gfwBarLoaded: false
+      contactOpen: false
     };
   }
 
-  componentDidMount(){
-    this.uglyHackToSetGFWBar(true);
-  }
-
   componentWillUnmount(){
-    this.uglyHackToSetGFWBar(false);
     window._babelPolyfill = false;
   }
 
-  uglyHackToSetGFWBar(open) {
-    this.header = document.getElementById('headerGfw');
-    if (this.header) {
-      if (open) {
-        this.header.style = 'height: auto; visibility: visible;';
-      } else {
-        this.header.style = `height: 0px; visibility: hidden;`;
-      }
-    }
-  }
-
-  handleLanguageChange(e) {
-    this.props.setLocale(e.value);
+  handleLanguageChange = (lang) => {
+    this.props.setLocale(lang);
   }
 
   render() {
     return (
       <div className="c-landing">
-        <div className="landing-nav">
-          {this.state.gfwBarLoaded && <div className="locale-container">
-            <div className="locale-select-container">
-              <Select
-                name="locale-select"
-                className="c-select"
-                classNamePrefix="Select"
-                value={{value: this.props.locale, label: this.props.locale.toUpperCase()}}
-                options={this.languages}
-                onChange={this.handleLanguageChange}
-                isSearchable={false}
-                components={{ DropdownIndicator }}
-              />
-            </div>
-          </div>}
-          <div id="loader-gfw" />
-          <Script
-            url={GFW_ASSETS_PATH}
-            onLoad={() => {this.setState({gfwBarLoaded: true })}}
-          />
-        </div>
+        <div id="headerGfw" />
+        <Script>
+          {window.gfwHeader = {
+            languages: this.languages,
+            afterLangSelect: this.handleLanguageChange,
+            customLogo: fwLogo
+          }}
+        </Script>
+        <Script
+          url={GFW_ASSETS_PATH}
+        />
         <div className="row landing-content">
           <div className="column align-middle small-12 medium-12 large-6 info-column gwf-grid-adjusted">
             <div className="main">
@@ -130,7 +102,8 @@ class Landing extends React.Component {
           </div>
         </div>
         <SocialFooter />
-        <div id="footerGfw"></div>
+        <div id="footerGfw" />
+        <div id="contactGfw" />
       </div>
     );
   }
