9022c68e8dbf7ea19028683d0db9a3ca16ebc113
Kevin Borrill <kevin.borrill@3sidedcube.com>
2022-03-17 10:05:10 +0000

    Create closed landing page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import querystring from 'query-string';
 import ReduxToastr from 'react-redux-toastr';
 import { IntlProvider } from 'react-intl';
@@ -11,20 +11,12 @@ import fr from 'react-intl/locale-data/fr';
 import id from 'react-intl/locale-data/id';
 import pt from 'react-intl/locale-data/pt';
 import translations from '../../locales/index.js';
-import { DEFAULT_LANGUAGE, GA_UA } from '../../constants/global';
-import ReactGA from 'react-ga';
+import { DEFAULT_LANGUAGE } from '../../constants/global';
 import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
 import Nav from '../layouts/Nav';
 
 // Pages
-import Login from '../../pages/login/Login';
-import Areas from '../../pages/areas/AreasContainer';
-import AreasManage from '../../pages/areas-manage/AreasManageContainer';
-import Templates from '../../pages/templates/TemplatesContainer';
-import TemplatesManage from '../../pages/templates-manage/TemplatesManageContainer';
-import Settings from '../../pages/settings/SettingsContainer';
-import Reports from '../../pages/reports/ReportsContainer';
-import Landing from '../../pages/landing/LandingContainer';
+import Landing from '../../pages/landing-closed/Landing';
 
 
 addLocaleData([...en, ...es, ...fr, ...id, ...pt]);
@@ -32,63 +24,8 @@ addLocaleData([...en, ...es, ...fr, ...id, ...pt]);
 
 class App extends React.Component {
 
-  componentWillMount() {
-    this.props.checkLogged(this.props.location.search);
-    ReactGA.initialize(GA_UA); //Unique Google Analytics tracking number
-  }
-
-  componentDidMount() {
-    this.checkConfirmedUser(this.props);
-    this.fireTracking(this.props.location);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const { location } = nextProps;
-    const queryParams = querystring.parse(nextProps.location.search);
-    const confirmToken = queryParams.confirmToken;
-    if ((nextProps.user.token !== this.props.user.token) && confirmToken){
-      this.checkConfirmedUser(nextProps);
-    }
-    if (location.pathname !== this.props.location.pathname || location.search !== this.props.location.search) {
-      this.fireTracking(nextProps.location);
-    }
-  }
-
-  fireTracking = (location) => {
-    ReactGA.set({ page: location.pathname + location.search });
-    ReactGA.pageview(location.pathname + location.search);
-  }
-
-  checkConfirmedUser(props) {
-    const queryParams = querystring.parse(props.location.search);
-    const confirmToken = queryParams.confirmToken;
-    if (confirmToken && props.user.token) this.props.confirmUser(confirmToken);
-  }
-
-  getLoginComponent = () => {
-    const { user, location } = this.props;
-    const search = location.search || '';
-    const queryParams = querystring.parse(search);
-    const callbackUrl = queryParams.callbackUrl;
-    const confirmToken = queryParams.confirmToken;
-    if (!user.loggedIn && queryParams.token && ((callbackUrl || confirmToken) )) {
-      return <Redirect to={{
-        pathname: '/login',
-        search: location.search
-      }}/>;
-    } else if (user.loggedIn) {
-      return <Redirect to="/areas" />;
-    } else {
-      return <Login callbackUrl={callbackUrl}/>;
-    }
-  }
-  redirectToLogin(){
-    return <Redirect to="/login" />;
-  }
   render() {
-    const { match, user, userChecked, logout, locale, setLocale } = this.props;
-    if (!userChecked) return null;
-    const queryParams = querystring.parse(window.location.search || '');
+    const { locale, setLocale } = this.props;
     const mergedMessages = Object.assign({}, translations[DEFAULT_LANGUAGE], translations[locale]);
     return (
       <IntlProvider
@@ -96,51 +33,11 @@ class App extends React.Component {
         messages={mergedMessages}
       >
         <div>
-          { ((user.loggedIn && window.location.pathname !== '/') || window.location.pathname === "/login") &&
-            <header className="l-header" role="banner">
-            <Nav
-              loggedIn={user.loggedIn}
-              logout={logout}
+          <main role="main" className="l-main">
+            <Landing
               locale={locale}
               setLocale={setLocale}
-              translations={translations}
-              user={user}
-            />
-          </header>}
-          <main role="main" className="l-main">
-            <Switch>
-              <Route
-                exact
-                path="/"
-                render={() => (
-                  <Landing
-                  locale={locale}
-                  setLocale={setLocale}
-                  translations={translations} />
-                )}
-              />
-              <Route path={`${match.url}login`} render={this.getLoginComponent} />
-              {user.loggedIn ?
-                <Switch>
-                  <Route exact path={`${match.url}areas`} component={Areas} />
-                  <Route exact path={`${match.url}areas/create`} component={AreasManage} />
-                  <Route exact path={`${match.url}areas/:areaId`} component={AreasManage} />
-                  <Route exact path={`${match.url}templates`} component={Templates} />
-                  <Route exact path={`${match.url}templates/create`} component={TemplatesManage} />
-                  <Route exact path={`${match.url}templates/:templateId`} component={TemplatesManage} />
-                  <Route exact path={`${match.url}reports`} component={Reports} />
-                  <Route path={`${match.url}reports/:templateId`} component={Reports} />
-                  <Route exact path={`${match.url}settings`} component={Settings} />
-                </Switch>
-                :
-                !queryParams.token && <Route path={`${match.url}`} render={this.redirectToLogin} />
-              }
-            </Switch>
-            <ReduxToastr
-              position="bottom-right"
-              transitionIn="fadeIn"
-              transitionOut="fadeOut"
-            />
+              translations={translations} />
           </main>
         </div>
       </IntlProvider>
---
+++ src/pages/landing-closed/Landing.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
+import { GFW_ASSETS_PATH, DOWNLOAD_APK_LINK, DOWNLOAD_APK_VERSION } from '../../constants/landing';
+import SocialFooter from '../landing/SocialFooter';
+import Select from 'react-select';
+import Script from 'react-load-script';
+import DropdownIndicator from '../../components/ui/SelectDropdownIndicator'
+import ReactGA from 'react-ga';
+
+class Landing extends React.Component {
+
+  static propTypes = {
+    locale: PropTypes.string,
+    setLocale: PropTypes.func,
+    translations: PropTypes.object
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleLanguageChange = this.handleLanguageChange.bind(this);
+    this.languages = props.translations && Object.keys(props.translations).map((lang) => (
+      { value: lang, label: lang.toUpperCase() }
+    ));
+    this.state = {
+      gfwBarLoaded: false
+    };
+  }
+
+  componentDidMount(){
+    this.uglyHackToSetGFWBar(true);
+  }
+
+  componentWillUnmount(){
+    this.uglyHackToSetGFWBar(false);
+    window._babelPolyfill = false;
+  }
+
+  uglyHackToSetGFWBar(open) {
+    this.header = document.getElementById('headerGfw');
+    if (this.header) {
+      if (open) {
+        this.header.style = 'height: auto; visibility: visible;';
+      } else {
+        this.header.style = `height: 0px; visibility: hidden;`;
+      }
+    }
+  }
+
+  handleLanguageChange(e) {
+    this.props.setLocale(e.value);
+  }
+
+  render() {
+    return (
+      <div className="c-landing">
+        <div className="landing-nav">
+          {this.state.gfwBarLoaded && <div className="locale-container">
+            <div className="locale-select-container">
+              <Select
+                name="locale-select"
+                className="c-select"
+                classNamePrefix="Select"
+                value={{value: this.props.locale, label: this.props.locale.toUpperCase()}}
+                options={this.languages}
+                onChange={this.handleLanguageChange}
+                isSearchable={false}
+                components={{ DropdownIndicator }}
+              />
+            </div>
+          </div>}
+          <div id="loader-gfw" />
+          <Script
+            url={GFW_ASSETS_PATH}
+            onLoad={() => {this.setState({gfwBarLoaded: true })}}
+          />
+        </div>
+        <div className="row landing-content">
+          <div className="column align-middle large-12 info-column gwf-grid-adjusted">
+            <div className="main">
+              <h1><FormattedMessage id="app.name" /></h1>
+              <h2><FormattedMessage id="app.closed" /></h2>
+            </div>
+          </div>
+        </div>
+        <SocialFooter />
+        <div id="footerGfw"></div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
