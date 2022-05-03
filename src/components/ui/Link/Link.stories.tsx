8a6c5593a59f5e143b03eb50953ffc3ed2e0b28e
Kevin Borrill <kevin.borrill@3sidedcube.com>
2022-05-03 18:27:08 +0100

    Add anchor example

--- src/components/ui/Link/Link.stories.tsx
+++ src/components/ui/Link/Link.stories.tsx
@@ -1,5 +1,5 @@
 import { ComponentStory, ComponentMeta } from "@storybook/react";
-import { AnchorHTMLAttributes, HTMLProps } from "react";
+import { FC } from "react";
 
 import { Link, BrowserRouter } from "react-router-dom";
 
@@ -10,10 +10,16 @@ export default {
 } as ComponentMeta<typeof Link>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-export const Example: ComponentStory<typeof Link> = args => (
+export const ReactRouter: ComponentStory<typeof Link> = args => (
   <BrowserRouter>
     <Link {...args} to="/" className="c-link">
       Click me
     </Link>
   </BrowserRouter>
+);
+
+export const AnchorElement: ComponentStory<FC> = args => (
+  <a {...args} href="/" className="c-link">
+    Click me
+  </a>
 );
