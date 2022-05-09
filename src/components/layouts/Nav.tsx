bfbf042d54681989734599584b64f47111fb4edb
Kevin Borrill <kevin.borrill@3sidedcube.com>
2022-05-09 16:44:10 +0100

    Update Navigation style

---
+++ src/components/layouts/Nav.tsx
@@ -0,0 +1,138 @@
+import { HTMLAttributes, useMemo, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import Select from "components/ui/Form/Select";
+
+import { MY_GFW_LINK } from "../../constants/global";
+import { FormattedMessage, useIntl } from "react-intl";
+import ReactGA from "react-ga";
+import classnames from "classnames";
+import Logo from "assets/images/Logo.svg";
+import ProfileIcon from "assets/images/icons/Profile.svg";
+
+import { FC } from "react";
+import { useForm } from "react-hook-form";
+
+interface IProps extends HTMLAttributes<HTMLDivElement> {
+  translations: any;
+  setLocale: (val: string) => void;
+  loggedIn: boolean;
+  logout: () => void;
+  locale: string;
+  user: any;
+}
+
+const Nav: FC<IProps> = props => {
+  const { translations, setLocale, loggedIn, logout, locale, user, ...rest } = props;
+  const formhook = useForm();
+  const { register, watch } = formhook;
+  const intl = useIntl();
+  const localeValue = watch("localeSelect");
+
+  useEffect(() => {
+    setLocale(localeValue);
+  }, [localeValue, setLocale]);
+
+  const languages = useMemo(
+    () =>
+      Object.keys(translations).map(lang => ({
+        value: lang,
+        label: intl.formatMessage({ id: `app.localeName.${lang}`, defaultMessage: lang })
+      })),
+    [intl, translations]
+  );
+
+  const username = (user.data && user.data.email) || <FormattedMessage id="app.setupEmail" />;
+
+  return (
+    <div className="row column" {...rest}>
+      <nav className="c-nav">
+        <h1>
+          <NavLink exact to="/" className="c-nav__logo" activeClassName="c-nav__logo--active">
+            <img src={Logo} alt="" role="presentation" />
+            <span className="u-visually-hidden">
+              <FormattedMessage id="app.name" />
+            </span>
+          </NavLink>
+        </h1>
+        <div className="c-nav__section">
+          {loggedIn && (
+            <ul className="c-nav__subsection c-nav__subsection--links">
+              <li className="c-nav__link-wrapper">
+                <NavLink to="/reports" className="c-nav__link" activeClassName="c-nav__link--active">
+                  <FormattedMessage id="reports.name" />
+                </NavLink>
+              </li>
+              <li className="c-nav__link-wrapper">
+                <NavLink to="/areas" className="c-nav__link" activeClassName="c-nav__link--active">
+                  <FormattedMessage id="areas.name" />
+                </NavLink>
+              </li>
+              <li className="c-nav__link-wrapper">
+                <NavLink to="/templates" className="c-nav__link" activeClassName="c-nav__link--active">
+                  <FormattedMessage id="templates.name" />
+                </NavLink>
+              </li>
+              <li className="c-nav__link-wrapper">
+                <NavLink to="/settings" className="c-nav__link" activeClassName="c-nav__link--active">
+                  <FormattedMessage id="teams.name" />
+                </NavLink>
+              </li>
+              <li className="c-nav__link-wrapper">
+                <NavLink to="/layers" className="c-nav__link" activeClassName="c-nav__link--active">
+                  <FormattedMessage id="layers.name" />
+                </NavLink>
+              </li>
+            </ul>
+          )}
+          <ul className={classnames("c-nav__subsection", loggedIn && "c-nav__subsection--settings")}>
+            <li className="c-nav__menu">
+              {/* <LegacySelect
+                name="locale-select"
+                className="c-select -dark"
+                classNamePrefix="Select"
+                value={{ value: locale, label: locale.toUpperCase() }}
+                options={languages}
+                onChange={handleLanguageChange}
+                isSearchable={false}
+                components={{ DropdownIndicator }}
+              /> */}
+              <Select
+                id="locale-select"
+                registered={register("localeSelect")}
+                formHook={formhook}
+                isSimple
+                selectProps={{
+                  placeholder: "",
+                  options: languages,
+                  label: "Select",
+                  defaultValue: languages[0]
+                }}
+              />
+            </li>
+            <li className="c-nav__link-wrapper">
+              <ReactGA.OutboundLink
+                eventLabel="navigation - myGFW"
+                to={MY_GFW_LINK}
+                rel="noopener noreferrer"
+                target="_blank"
+                className="c-nav__link"
+              >
+                <img src={ProfileIcon} alt="" role="presentation" className="c-nav__link-profile-icon" /> {username}
+              </ReactGA.OutboundLink>
+            </li>
+
+            {loggedIn && (
+              <li className="c-nav__link-wrapper">
+                <button onClick={logout} className="c-nav__link">
+                  <FormattedMessage id="app.logout" />
+                </button>
+              </li>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
--- src/components/ui/Form/Select.tsx
+++ src/components/ui/Form/Select.tsx
@@ -11,11 +11,12 @@ export interface Props extends FieldPropsBase {
   selectProps: SelectProps;
   registered: UseFormRegisterReturn;
   formHook: Pick<UseFormReturn, "watch" | "setValue" | "clearErrors">;
-  onChange: () => void;
+  onChange?: () => void;
+  isSimple: boolean;
 }
 
 const Select = (props: Props) => {
-  const { selectProps, registered, error, id, formHook } = props;
+  const { selectProps, registered, error, id, formHook, isSimple } = props;
   const [options, setOptions] = useState<Option[]>(selectProps.options || []);
   const value = formHook.watch(props.registered.name) || selectProps.defaultValue?.value;
 
@@ -30,7 +31,9 @@ const Select = (props: Props) => {
     if (v.value) {
       props.formHook.setValue(registered.name, v?.value);
       props.formHook.clearErrors(registered.name);
-      props.onChange();
+      if (props.onChange) {
+        props.onChange();
+      }
     }
   };
 
@@ -47,20 +50,28 @@ const Select = (props: Props) => {
               <Listbox.Label
                 className={classnames(
                   "c-input__label c-input__label--select",
-                  selectProps.alternateLabelStyle && "c-input__label--alt"
+                  selectProps.alternateLabelStyle && "c-input__label--alt",
+                  isSimple && "u-visually-hidden"
                 )}
               >
                 {selectProps.label}
               </Listbox.Label>
             )}
             <div>
-              <div className={classnames("c-input__select", open && "c-input__select--open")}>
+              <div
+                className={classnames(
+                  "c-input__select",
+                  open && "c-input__select--open",
+                  isSimple && "c-input__select--simple"
+                )}
+              >
                 <Listbox.Button
                   className={classnames(
                     "c-input__select-button",
                     !value && "c-input__select-button--has-placeholder",
                     open && "c-input__select-button--open",
-                    error && "c-input__select-button--invalid"
+                    error && "c-input__select-button--invalid",
+                    isSimple && "c-input__select-button--simple"
                   )}
                 >
                   <span className="c-input__select-value">
@@ -68,6 +79,27 @@ const Select = (props: Props) => {
                   </span>
                   {error ? (
                     <img alt="" role="presentation" src={errorIcon} className="c-input__select-error-icon" />
+                  ) : isSimple ? (
+                    <svg
+                      width="10"
+                      height="11"
+                      viewBox="0 0 10 11"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className={classnames("c-input__select-indicator", open && "c-input__select-indicator--rotate")}
+                    >
+                      <g clip-path="url(#clip0_564_444)">
+                        <path
+                          d="M9.90031 3.05438L9.39937 2.55344C9.3325 2.48656 9.25562 2.45312 9.16875 2.45312C9.08187 2.45312 9.00531 2.48656 8.93844 2.55344L5.00031 6.49125L1.0625 2.55344C0.995625 2.48656 0.91875 2.45312 0.831875 2.45312C0.745 2.45312 0.668125 2.48656 0.60125 2.55344L0.100312 3.05438C0.0334375 3.12125 0 3.19813 0 3.285C0 3.37187 0.0334375 3.44875 0.100312 3.51531L4.76969 8.18469C4.83656 8.25156 4.91344 8.285 5.00031 8.285C5.08719 8.285 5.16375 8.25156 5.23062 8.18469L9.90031 3.51531C9.96719 3.44844 10.0003 3.37156 10.0003 3.285C10.0003 3.19844 9.96719 3.12125 9.90031 3.05438Z"
+                          fill="#555555"
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_564_444">
+                          <rect width="10" height="10" fill="white" transform="translate(0 0.369141)" />
+                        </clipPath>
+                      </defs>
+                    </svg>
                   ) : (
                     <svg
                       width="17"
@@ -85,7 +117,9 @@ const Select = (props: Props) => {
                     </svg>
                   )}
                 </Listbox.Button>
-                <Listbox.Options className="c-input__select-list-box">
+                <Listbox.Options
+                  className={classnames("c-input__select-list-box", isSimple && "c-input__select-list-box--simple")}
+                >
                   {options.map(option => (
                     <Listbox.Option as={Fragment} key={option.value} value={option}>
                       {({ active, selected }) => (
@@ -93,7 +127,8 @@ const Select = (props: Props) => {
                           className={classnames(
                             "c-input__select-list-item",
                             active && "c-input__select-list-item--is-active",
-                            selected && "c-input__select-list-item--is-selected"
+                            selected && "c-input__select-list-item--is-selected",
+                            isSimple && "c-input__select-list-item--simple"
                           )}
                         >
                           {option.label}
