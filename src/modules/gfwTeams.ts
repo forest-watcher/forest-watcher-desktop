108052fe7a09152ae22621a41b7c50cd155df43d
owenjs <owen.evans115@gmail.com>
2022-05-19 13:29:27 +0100

    getTeamMembers service added

--- src/modules/gfwTeams.ts
+++ src/modules/gfwTeams.ts
@@ -1,23 +1,36 @@
 import { teamService } from "services/teams";
 import { RootState, AppDispatch } from "index";
-import { TGetUserTeamsResponse } from "services/teams";
+import { TGetUserTeamsResponse, TGetTeamMembersResponse } from "services/teams";
 
 const GET_USER_TEAMS = "gfwTeams/GET_USER_TEAMS";
+const GET_TEAM_MEMBERS = "gfwTeams/GET_TEAM_MEMBERS";
 
 export type TGFWTeamsState = {
   data: TGetUserTeamsResponse["data"];
+  members: { [teamId: string]: TGetTeamMembersResponse["data"] };
 };
 
-export type TReducerActions = { type: typeof GET_USER_TEAMS; teams: TGFWTeamsState["data"] };
+export type TReducerActions =
+  | { type: typeof GET_USER_TEAMS; payload: { teams: TGFWTeamsState["data"] } }
+  | { type: typeof GET_TEAM_MEMBERS; payload: { teamId: string; members: TGFWTeamsState["members"][string] } };
 
 const initialState: TGFWTeamsState = {
-  data: []
+  data: [],
+  members: {}
 };
 
 export default function reducer(state = initialState, action: TReducerActions) {
   switch (action.type) {
     case GET_USER_TEAMS: {
-      return Object.assign({}, state, { data: action.teams });
+      return Object.assign({}, state, { data: action.payload.teams });
+    }
+    case GET_TEAM_MEMBERS: {
+      const teamMembers = {
+        ...state.members,
+        [action.payload.teamId]: action.payload.members
+      };
+
+      return Object.assign({}, state, { members: teamMembers });
     }
     default:
       return state;
@@ -33,7 +46,26 @@ export const getUserTeams = (userId: string) => (dispatch: AppDispatch, getState
     .then(({ data }) =>
       dispatch({
         type: GET_USER_TEAMS,
-        teams: data
+        payload: { teams: data }
+      })
+    )
+    .catch(error => {
+      console.warn("error", error);
+    });
+};
+
+export const getTeamMembers = (teamId: string) => (dispatch: AppDispatch, getState: () => RootState) => {
+  teamService.token = getState().user.token;
+
+  teamService
+    .getTeamMembers(teamId)
+    .then(({ data }) =>
+      dispatch({
+        type: GET_USER_TEAMS,
+        payload: {
+          teamId: teamId,
+          members: data
+        }
       })
     )
     .catch(error => {
