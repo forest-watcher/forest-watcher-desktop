2228a7183253ccbafc513c70bdbb5d29a2f7587c
owenjs <owen.evans115@gmail.com>
2022-05-25 13:09:19 +0100

    team detail page displays team and the members - not styled

--- src/pages/teams-detail/TeamDetail.tsx
+++ src/pages/teams-detail/TeamDetail.tsx
@@ -1,6 +1,8 @@
-import { FC } from "react";
+import { FC, useMemo } from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { TPropsFromRedux } from "./TeamDetailContainer";
+import Hero from "../../components/layouts/Hero";
+import Article from "../../components/layouts/Article";
 
 type TParams = {
   teamId: string;
@@ -11,9 +13,64 @@ export interface IOwnProps extends RouteComponentProps<TParams> {}
 type IProps = IOwnProps & TPropsFromRedux;
 
 const TeamDetail: FC<IProps> = props => {
-  const { team } = props;
+  const { team, teamMembers } = props;
 
-  return <div>{JSON.stringify(team)}</div>;
+  // ToDo: Create a util for this
+  const [manages, monitors] = useMemo(
+    () =>
+      teamMembers.reduce<[typeof teamMembers, typeof teamMembers]>(
+        (acc, teamMember) => {
+          if (
+            (teamMember.attributes.role === "administrator" || teamMember.attributes.role === "manager") &&
+            teamMember.attributes.userId
+          ) {
+            acc[0].push(teamMember);
+          } else if (teamMember.attributes.userId) {
+            acc[1].push(teamMember);
+          }
+          return acc;
+        },
+        [[], []]
+      ),
+    [teamMembers]
+  );
+
+  const deleteTeam = () => {
+    // ToDo
+  };
+
+  // ToDo: On initial load team and members won't have been fetched
+  if (!team || !teamMembers) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <Hero
+        title="teams.details.name"
+        titleValues={{ name: team.attributes.name }}
+        action={{ name: "teams.details.delete", callback: deleteTeam }}
+      />
+      <div className="l-content c-teams-details">
+        <Article
+          className="c-teams-details__heading"
+          title="teams.details.managers"
+          titleValues={{ num: manages.length }}
+        >
+          <div>{JSON.stringify(manages)}</div>
+        </Article>
+      </div>
+      <div className="l-content c-teams-details">
+        <Article
+          className="c-teams-details__heading"
+          title="teams.details.monitors"
+          titleValues={{ num: monitors.length }}
+        >
+          <div>{JSON.stringify(monitors)}</div>
+        </Article>
+      </div>
+    </>
+  );
 };
 
 export default TeamDetail;
--- src/pages/teams-detail/TeamDetailContainer.tsx
+++ src/pages/teams-detail/TeamDetailContainer.tsx
@@ -3,7 +3,8 @@ import { RootState } from "index";
 import TeamDetail, { IOwnProps } from "./TeamDetail";
 
 const mapStateToProps = ({ gfwTeams }: RootState, ownProps: IOwnProps) => ({
-  team: gfwTeams.data.filter(team => team.id === ownProps.match.params.teamId)
+  team: gfwTeams.data.find(team => team.id === ownProps.match.params.teamId),
+  teamMembers: gfwTeams.members[ownProps.match.params.teamId]
 });
 
 const mapDispatchToProps = {};
