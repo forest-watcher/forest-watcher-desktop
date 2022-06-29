faff4a1f4055704edd91173be60ff25a935220a4
owenjs <owen.evans115@gmail.com>
2022-06-29 13:08:14 +0100

    redux debugger removed from production

--- src/configureStore.tsx
+++ src/configureStore.tsx
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { autoRehydrate } from "redux-persist";
+import { ENVIRONMENT } from "constants/global";
 
 import * as reducers from "./modules";
 import "./index.scss";
@@ -14,7 +15,8 @@ const reducer = combineReducers({
   ...reducers
 });
 
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  ENVIRONMENT !== "production" ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose : compose;
 const configureStore = () => createStore(reducer, composeEnhancers(applyMiddleware(thunk), autoRehydrate()));
 
 export default configureStore;
