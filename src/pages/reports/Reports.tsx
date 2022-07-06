f4d6842580820836656663d4862f9664581250f4
Kevin Borrill <kevin.borrill@3sidedcube.com>
2022-07-06 10:15:11 +0100

    only use l-reports for investigation page.

--- src/pages/reports/Reports.tsx
+++ src/pages/reports/Reports.tsx
@@ -5,12 +5,16 @@ import Hero from "components/layouts/Hero/Hero";
 import { IProps as ITabGroupProps } from "components/ui/TabGroup/TabGroup";
 import InvestigationPage from "./investigation/Investigation";
 import ReportsPage from "./reports/ReportsContainer";
+import classnames from "classnames";
 
 type TParams = {
   reportingTab: string;
 };
 
 interface IProps extends TPropsFromRedux, RouteComponentProps<TParams> {}
+
+const INVESTIGATION_URL = "/reporting/investigation";
+const REPORTS_URL = "/reporting/reports";
 
 const pageTabs: ITabGroupProps["options"] = [
   {
@@ -30,14 +34,14 @@ const Reports: FC<IProps> = props => {
   const { reportingTab } = match.params;
 
   return !pageTabs.find(pageTab => pageTab.value === reportingTab) ? (
-    <Redirect to={`/reporting/investigation`} />
+    <Redirect to={INVESTIGATION_URL} />
   ) : (
-    <div className="l-reporting">
+    <div className={classnames(match.url === INVESTIGATION_URL && "l-reporting")}>
       <Hero title="reports.name" pageTabs={{ value: reportingTab, options: pageTabs }} />
 
       <Switch>
-        <Route path={"/reporting/investigation"} component={InvestigationPage} />
-        <Route path={"/reporting/reports"} component={ReportsPage} />
+        <Route path={INVESTIGATION_URL} component={InvestigationPage} />
+        <Route path={REPORTS_URL} component={ReportsPage} />
       </Switch>
     </div>
   );
