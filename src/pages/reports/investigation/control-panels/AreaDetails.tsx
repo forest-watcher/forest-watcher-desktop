a576a4769b1e0ebb85b37e014824c38a5029b78b
owenjs <owen.evans115@gmail.com>
2022-07-07 12:24:20 +0100

    Revert "Revert "area id not found now errors on reporting/investigation page""
    This reverts commit 9b859b6d1ec43dde9a24f477d467235e6a54d659.

--- src/pages/reports/investigation/control-panels/AreaDetails.tsx
+++ src/pages/reports/investigation/control-panels/AreaDetails.tsx
@@ -1,4 +1,6 @@
-import { FC, useEffect, useMemo } from "react";
+import { FC, useCallback, useEffect, useMemo } from "react";
+import { useIntl } from "react-intl";
+import { toastr } from "react-redux-toastr";
 import { RouteComponentProps } from "react-router-dom";
 import { LngLatBoundsLike, useMap } from "react-map-gl";
 import * as turf from "@turf/turf";
@@ -13,6 +15,7 @@ interface IProps extends RouteComponentProps<TParams> {}
 const AreaDetailsControlPanel: FC<IProps> = props => {
   const { match, history } = props;
   const { areaId } = match.params;
+  const intl = useIntl();
   const { data: areas, loading: isLoadingAreas } = useAppSelector(state => state.areas);
   const { current: map } = useMap();
 
@@ -29,9 +32,18 @@ const AreaDetailsControlPanel: FC<IProps> = props => {
     }
   }, [map, bounds]);
 
-  const handleBackBtnClick = () => {
+  const handleBackBtnClick = useCallback(() => {
     history.push("/reporting/investigation");
-  };
+  }, [history]);
+
+  useEffect(() => {
+    // If the areas has been fetched, and the selected Area Geo Data hasn't
+    // been found then return to reporting/investigation as the areaId is invalid
+    if (!selectedAreaGeoData && areaId && Object.keys(areas).length) {
+      toastr.warning(intl.formatMessage({ id: "reporting.investigation.error" }), "");
+      handleBackBtnClick();
+    }
+  }, [selectedAreaGeoData, areaId, areas, handleBackBtnClick, intl]);
 
   return (
     <MapCard className="c-map-control-panel" title={areas[areaId]?.attributes.name} onBack={handleBackBtnClick}>
