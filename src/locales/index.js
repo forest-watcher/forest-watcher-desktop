ef9151e72bc061edd5ac21e283ab93c29e001ad5
Kevin Borrill <kevin.borrill@3sidedcube.com>
2022-07-26 14:04:50 +0100

    Add new languages

--- src/locales/index.js
+++ src/locales/index.js
@@ -4,13 +4,17 @@ import es from "./es.json";
 import fr from "./fr.json";
 import id from "./id.json";
 import pt from "./pt.json";
+import nl from "./nl.json";
+import mg from "./mg.json";
 
 const translations = {
   en: { ...en },
   es: { ...es },
   fr: { ...fr },
   id: { ...id },
-  pt: { ...pt }
+  pt: { ...pt },
+  nl: { ...nl },
+  mg: { ...mg }
 };
 
 export default translations;
