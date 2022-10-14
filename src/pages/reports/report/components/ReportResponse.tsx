3f9d4df1615e3716658469b8abe4dcd89308ad05
Gianmarco Ciaffardini <marco@Gianmarcos-MBP.homenet.telecomitalia.it>
2022-10-14 16:30:49 +0200

    chore: add borders

--- src/pages/reports/report/components/ReportResponse.tsx
+++ src/pages/reports/report/components/ReportResponse.tsx
@@ -21,11 +21,17 @@ const ReportResponse = ({ question, response, type }: ReportResponseProps) => {
   return (
     <section className="my-10">
       {/* Question */}
-      <div className="bg-green-400 py-7 px-6">
+      <div className="bg-green-400 border-2 border-solid border-green-500 py-7 px-6 rounded-t-[4px] border-opacity-20">
         <p className="text-[24px] text-gray-700">{question}</p>
       </div>
       {/* Response */}
-      <div className={`${type === "blob" && response !== null ? "" : "bg-gray-300 py-7 px-6"}`}>
+      <div
+        className={`${
+          type === "blob" && response !== null
+            ? ""
+            : "bg-gray-300 py-7 px-6 border-2 border-solid border-gray-500 border-opacity-40 rounded-b-[4px]"
+        }`}
+      >
         {_renderResponse()}
       </div>
     </section>
