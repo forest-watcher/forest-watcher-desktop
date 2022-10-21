f80a10cf8ed2020d8a54f67f757412c120bb96e8
owenjs <owen.evans115@gmail.com>
2022-10-21 08:51:55 +0100

    icon bubble added to empty state

--- src/pages/reports/investigation/control-panels/AddAssignment/states/EmptyState.tsx
+++ src/pages/reports/investigation/control-panels/AddAssignment/states/EmptyState.tsx
@@ -1,6 +1,7 @@
 import Icon from "components/extensive/Icon";
 import List from "components/extensive/List";
 import Button from "components/ui/Button/Button";
+import IconBubble from "components/ui/Icon/IconBubble";
 import { FC } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
 
@@ -11,15 +12,15 @@ const OpenAssignmentEmptyState: FC<IProps> = props => {
   const intl = useIntl();
 
   return (
-    <div className="rounded-md bg-gray-400 px-4 py-6 flex flex-col items-center gap-y-6">
-      <Icon name="flag" />
+    <div className="rounded-md bg-gray-400 px-4 py-6 flex flex-col items-center">
+      <IconBubble className="mb-3" name="flag-white" size={22} />
 
-      <h1 className="text-lg text-gray-700 text-center">
+      <h1 className="text-lg text-gray-700 text-center mb-6">
         <FormattedMessage id="assignments.empty.title" />
       </h1>
 
       <List<{ text: string }>
-        className="flex flex-col text-base text-gray-700 gap-y-4"
+        className="flex flex-col text-base text-gray-700 gap-y-4 mb-6"
         items={[
           { text: intl.formatMessage({ id: "assignments.empty.content.list.item.1" }) },
           { text: intl.formatMessage({ id: "assignments.empty.content.list.item.2" }) },
