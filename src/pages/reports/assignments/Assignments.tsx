300043813b5c910f7ec27e350d9e9bc352d6dc58
Gianmarco Ciaffardini <marco@Gianmarcos-MBP.homenet.telecomitalia.it>
2022-11-03 12:27:03 +0100

    feat: add assignments table

---
+++ src/pages/reports/assignments/Assignments.tsx
@@ -0,0 +1,100 @@
+import LoadingWrapper from "components/extensive/LoadingWrapper";
+import Article from "components/layouts/Article";
+import Button from "components/ui/Button/Button";
+import DataTable from "components/ui/DataTable/DataTable";
+import { useGetV3GfwUser } from "generated/core/coreComponents";
+import { sortByDateString, sortByString } from "helpers/table";
+import { useAccessToken } from "hooks/useAccessToken";
+import { useMemo, useState } from "react";
+import { FormattedMessage, useIntl } from "react-intl";
+
+export type TAssignmentsDataTable = {
+  id: string;
+  createdAt: string;
+  area: string;
+  alertType: any;
+  status: string;
+  priority: string;
+};
+
+const Assignments = () => {
+  const intl = useIntl();
+  const { httpAuthHeader } = useAccessToken();
+  const [selectedAssignments, setSelectedAssignments] = useState<TAssignmentsDataTable[]>([]);
+
+  // Queries - Get User's Assignments
+  const { data: assignmentsData, isLoading: assignmentsLoading } = useGetV3GfwUser({ headers: httpAuthHeader });
+
+  const rows = useMemo<TAssignmentsDataTable[]>(() => {
+    if (!assignmentsData?.data) return [];
+    return assignmentsData?.data?.map(assignment => ({
+      id: assignment.id ?? "",
+      createdAt: assignment.attributes?.createdAt ?? "",
+      area: assignment.attributes?.areaName ?? "-",
+      alertType: assignment.attributes?.location,
+      priority: assignment.attributes?.priority ?? "",
+      status: assignment.attributes?.status ?? ""
+    })) as TAssignmentsDataTable[];
+  }, [assignmentsData]);
+
+  return (
+    <div className="l-content">
+      <LoadingWrapper loading={assignmentsLoading}>
+        <Article
+          title="reporting.tabs.assignments"
+          size="small"
+          actions={
+            <Button>
+              <FormattedMessage id={selectedAssignments.length > 0 ? "export.selected" : "export.all"} />
+            </Button>
+          }
+        >
+          <DataTable
+            className="u-w-100"
+            rows={rows}
+            isPaginated
+            onSelect={setSelectedAssignments}
+            selectFindGetter="id"
+            columnOrder={[
+              {
+                key: "createdAt",
+                name: "assignments.table.createdAt",
+                rowLabel: (row, value) => {
+                  return !Array.isArray(value)
+                    ? intl.formatDate(value, { day: "2-digit", month: "2-digit", year: "2-digit" })
+                    : "";
+                },
+                sortCompareFn: sortByDateString
+              },
+              { key: "area", name: "assignments.table.area", sortCompareFn: sortByString },
+              {
+                key: "alertType",
+                name: "assignments.table.alertType",
+                rowLabel: (row, value) => {
+                  if (!Array.isArray(value)) {
+                    return "";
+                  }
+                  if (value.length === 0) {
+                    return intl.formatMessage({ id: "layers.none" });
+                  }
+
+                  return value.map(alert => intl.formatMessage({ id: `layers.${alert.alertType}` })).join(", ");
+                },
+                sortCompareFn: (a, b, direction) => {
+                  const newA = intl.formatMessage({ id: `layers.${a}`, defaultMessage: a?.toString() });
+                  const newB = intl.formatMessage({ id: `layers.${b}`, defaultMessage: b?.toString() });
+
+                  return sortByString(newA, newB, direction);
+                }
+              },
+              { key: "status", name: "assignments.table.status", sortCompareFn: sortByString },
+              { key: "priority", name: "assignments.table.priority", sortCompareFn: sortByString }
+            ]}
+          />
+        </Article>
+      </LoadingWrapper>
+    </div>
+  );
+};
+
+export default Assignments;
