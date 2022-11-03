5e6eae37c2c9d9d7c0218e7defa5b87614b74704
Gianmarco Ciaffardini <marco@Gianmarcos-MBP.homenet.telecomitalia.it>
2022-11-03 13:55:18 +0100

    fix: font size

--- src/pages/reports/report/components/ReportDetails.tsx
+++ src/pages/reports/report/components/ReportDetails.tsx
@@ -14,7 +14,7 @@ const ReportDetails = ({ answer, report }: ReportDetailsProps) => {
 
   return (
     <section className="row column py-section">
-      <h1 className="font-base text-[36px] font-light text-gray-700 mb-10">{answer?.attributes?.reportName}</h1>
+      <h1 className="font-base text-4xl font-light text-gray-700 mb-10">{answer?.attributes?.reportName}</h1>
       <section className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
         <IconCard iconName={"check"} title="Completed Date" text={moment(createdAt).format("MMM DD, YYYY")} />
         <IconCard iconName={"footprint"} title="Monitor" text={fullName} />
