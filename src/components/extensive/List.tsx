d74a517d37e608de37f0a2cc1d4b4b0a85fc5158
Kevin Borrill <kevin.borrill@3sidedcube.com>
2022-11-22 16:25:17 +0000

    Fix template issues

--- src/components/extensive/List.tsx
+++ src/components/extensive/List.tsx
@@ -6,10 +6,10 @@ type ListProps<T> = {
   itemClassName?: string;
 };
 
-const List = <T extends Record<any, any>>({ items, render, className, itemClassName }: ListProps<T>) => {
+const List = <T extends Record<any, any>>({ items = [], render, className, itemClassName }: ListProps<T>) => {
   return (
     <ul className={className}>
-      {items?.map((item, i) => (
+      {items?.map?.((item, i) => (
         <li key={i} className={itemClassName}>
           {render(item, i)}
         </li>
