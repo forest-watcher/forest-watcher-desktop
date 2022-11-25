76cd8c24289a1f13a632ecdfe7a66e05646e2ac0
Kevin Borrill <kevin.borrill@3sidedcube.com>
2022-11-25 10:43:18 +0000

    Update carousel

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -4,7 +4,9 @@ import CarouselThumb from "./CarouselThumb";
 import Icon from "components/extensive/Icon";
 import OptionalWrapper from "components/extensive/OptionalWrapper";
 import CarouselImageDownloadModal from "./modal/CarouselImageDownloadModal";
-import { useIntl } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
+import Button from "components/ui/Button/Button";
+import downloadIcon from "assets/images/icons/Download.svg";
 
 type CarouselProps = {
   slides: string[];
@@ -13,79 +15,133 @@ type CarouselProps = {
 
 const Carousel = ({ slides, downloadable }: CarouselProps) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const [imageToDownload, setImageToDownload] = useState<string>("");
+  const [prevBtnEnabled, setPrevBtnEnabled] = useState(false);
+  const [nextBtnEnabled, setNextBtnEnabled] = useState(false);
+  const [imageToView, setImageToView] = useState<string>("");
   const intl = useIntl();
   const newSlides = [...slides, ...slides, ...slides, ...slides, ...slides, ...slides, ...slides, ...slides, ...slides];
-  const [mainViewportRef, carousel] = useEmblaCarousel({ skipSnaps: false });
+  const [mainViewportRef, carousel] = useEmblaCarousel({ skipSnaps: false, draggable: false });
   const [thumbViewportRef, carouselThumbs] = useEmblaCarousel({
     containScroll: "keepSnaps",
     dragFree: true
   });
 
+  const scrollPrev = useCallback(() => carousel?.scrollPrev(), [carousel]);
+  const scrollNext = useCallback(() => carousel?.scrollNext(), [carousel]);
+
+  const onSelect = useCallback(() => {
+    if (!carousel || !carouselThumbs) {
+      return;
+    }
+    setSelectedIndex(carousel.selectedScrollSnap());
+    carouselThumbs.scrollTo(carousel.selectedScrollSnap());
+
+    setPrevBtnEnabled(carousel.canScrollPrev());
+    setNextBtnEnabled(carousel.canScrollNext());
+  }, [carousel, carouselThumbs, setSelectedIndex]);
+
   const onThumbClick = useCallback(
     (index: number) => {
-      if (!carousel || !carouselThumbs) return;
-      if (carouselThumbs.clickAllowed()) carousel.scrollTo(index);
+      if (!carousel || !carouselThumbs) {
+        return;
+      }
+      if (carouselThumbs.clickAllowed()) {
+        carousel.scrollTo(index);
+      }
     },
     [carousel, carouselThumbs]
   );
-
-  const onSelect = useCallback(() => {
-    if (!carousel || !carouselThumbs) return;
-    setSelectedIndex(carousel.selectedScrollSnap());
-    carouselThumbs.scrollTo(carousel.selectedScrollSnap());
-  }, [carousel, carouselThumbs, setSelectedIndex]);
 
   useEffect(() => {
     if (!carousel) return;
     onSelect();
     carousel.on("select", onSelect);
+
+    return () => {
+      carousel.off("select", onSelect);
+    };
   }, [carousel, onSelect]);
 
+  const handleDownload = (url: string) => {
+    window.open(url, "_blank");
+  };
+
   if (!newSlides || newSlides.length === 0) {
-    return <p>No Images</p>;
+    return <FormattedMessage id="common.noImages" />;
   }
 
   return (
     <>
-      <OptionalWrapper data={!!imageToDownload}>
-        <CarouselImageDownloadModal onClose={() => setImageToDownload("")} imageToDownload={imageToDownload} />
+      <OptionalWrapper data={!!imageToView}>
+        <CarouselImageDownloadModal onClose={() => setImageToView("")} imageToDownload={imageToView} />
       </OptionalWrapper>
-      <div className="c-carousel pb-[25px]">
-        <div className="c-carousel__viewport" ref={mainViewportRef}>
+      <div className="c-carousel">
+        <div className="c-carousel__viewport mb-4 rounded-b" ref={mainViewportRef}>
           <div className="c-carousel__container">
             {newSlides.map((src, index) => (
               <div className="c-carousel__slide" key={index}>
                 <div className="relative h-[450px] overflow-hidden">
                   <OptionalWrapper data={downloadable}>
-                    <button
+                    {/* <button
                       className="absolute top-10 right-10 bg-primary-500 rounded-full z-10 w-10 h-10"
-                      onClick={() => setImageToDownload(src)}
+                      onClick={() => handleDownload(src)}
                       aria-label={intl.formatMessage({ id: "common.download" })}
                     >
                       <Icon name="download" size={36} />
-                    </button>
+                    </button> */}
+                    <Button
+                      className="absolute top-10 right-10 z-10"
+                      onClick={() => handleDownload(src)}
+                      aria-label={intl.formatMessage({ id: "common.download" })}
+                      isIcon
+                    >
+                      {/* <img src={downloadIcon} role="presentation" alt="" /> */}
+                      <Icon name="Download" size={36} />
+                    </Button>
                   </OptionalWrapper>
-                  <img className="w-full h-full object-cover" src={src} alt="" />
+                  <button
+                    className="w-full h-full"
+                    onClick={() => setImageToView(src)}
+                    aria-label={intl.formatMessage({ id: "common.view" })}
+                  >
+                    <img className="w-full h-full object-cover" src={src} alt="" />
+                  </button>
                 </div>
               </div>
             ))}
           </div>
         </div>
-      </div>
-
-      <div className="c-carousel c-carousel--thumb">
-        <div className="c-carousel__viewport" ref={thumbViewportRef}>
-          <div className="c-carousel__container c-carousel__container--thumb">
-            {newSlides.map((src, index) => (
-              <CarouselThumb
-                onClick={() => onThumbClick(index)}
-                selected={index === selectedIndex}
-                imgSrc={src}
-                key={index}
-              />
-            ))}
+        <div className="relative">
+          <Button
+            className="absolute -left-14 top-1/2 u-translate-y-half"
+            disabled={!prevBtnEnabled}
+            onClick={scrollPrev}
+            isIcon
+            aria-label={intl.formatMessage({ id: "common.previous" })}
+          >
+            <Icon name="ChevronLeft" size={36} />
+          </Button>
+          <div className="overflow-hidden" ref={thumbViewportRef}>
+            <div className="flex gap-4">
+              {newSlides.map((src, index) => (
+                <CarouselThumb
+                  onClick={() => onThumbClick(index)}
+                  selected={index === selectedIndex}
+                  imgSrc={src}
+                  key={index}
+                />
+              ))}
+            </div>
           </div>
+          <Button
+            className="absolute -right-14 top-1/2 u-translate-y-half"
+            disabled={!nextBtnEnabled}
+            onClick={scrollNext}
+            isIcon
+            aria-label={intl.formatMessage({ id: "common.next" })}
+          >
+            <Icon name="ChevronRight" size={36} />
+          </Button>
         </div>
       </div>
     </>
