182c4e0b357a90e66874ac348a8b9412d4230035
Kevin Borrill <kevin.borrill@3sidedcube.com>
2022-12-06 16:19:02 +0000

    Modal player

--- src/pages/reports/report/components/ReportResponse.tsx
+++ src/pages/reports/report/components/ReportResponse.tsx
@@ -1,6 +1,10 @@
 import Carousel from "components/carousel/Carousel";
 import OptionalWrapper from "components/extensive/OptionalWrapper";
 import HeaderCard from "components/ui/Card/HeaderCard";
+import Modal from "components/ui/Modal/Modal";
+import { download } from "helpers/exports";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export interface IReportResponse {
   question: string;
@@ -12,20 +16,36 @@ interface ReportResponseProps extends IReportResponse {}
 
 const Response = (props: ReportResponseProps) => {
   const { response, type } = props;
+  const [audioModalOpen, setAudioModalOpen] = useState(false);
   switch (type) {
     case "audio":
       const filename = response.split("/").pop();
 
       return (
-        <a
-          href={response}
-          target="_blank"
-          rel="noreferrer"
-          download={false}
-          className="bg-primary-400 px-4 py-[9px] rounded-md border border-solid border-primary-500 text-neutral-700 text-base"
-        >
-          {filename}
-        </a>
+        <>
+          <button
+            onClick={() => {
+              setAudioModalOpen(true);
+            }}
+            className="bg-primary-400 px-4 py-[9px] rounded-md border border-solid border-primary-500 text-neutral-700 text-base"
+          >
+            {filename}
+          </button>
+          <Modal
+            isOpen={audioModalOpen}
+            title="audio.play"
+            onClose={() => setAudioModalOpen(false)}
+            actions={[
+              { name: "common.download", onClick: () => download(response) },
+              { name: "common.close", variant: "secondary", onClick: () => setAudioModalOpen(false) }
+            ]}
+          >
+            <div className="w-full h-full flex justify-center align-middle">
+              {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+              <audio src={response} autoPlay controls />
+            </div>
+          </Modal>
+        </>
       );
     case "blob":
       // @ts-ignore response in this case is an array of strings.
