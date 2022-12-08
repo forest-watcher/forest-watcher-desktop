d2a93ace0a62f25a4db352dcaa8a7bf998d68955
Kevin Borrill <kevin.borrill@3sidedcube.com>
2022-12-08 12:55:20 +0000

    Create a route card

--- src/pages/reports/investigation/components/AreaRoutesSource.tsx
+++ src/pages/reports/investigation/components/AreaRoutesSource.tsx
@@ -1,13 +1,23 @@
 import * as turf from "@turf/turf";
+import MapCard from "components/ui/Map/components/cards/MapCard";
 import { linePointStyle, lineStyle } from "components/ui/Map/components/layers/styles";
 import { useGetV3GfwRoutesTeams, useGetV3GfwRoutesUser } from "generated/core/coreComponents";
+import { RouteResponse } from "generated/core/coreResponses";
 import { RouteModel } from "generated/core/coreSchemas";
+import { capitalizeFirstLetter } from "helpers/string";
 import { useAccessToken } from "hooks/useAccessToken";
-import { FC, useMemo } from "react";
-import { Layer, Source } from "react-map-gl";
+import { EventData, MapMouseEvent } from "mapbox-gl";
+import moment from "moment";
+import { FC, useEffect, useMemo, useState } from "react";
+import { FormattedMessage, useIntl } from "react-intl";
+import { Layer, Source, useMap } from "react-map-gl";
 
 export interface IProps {
   areaId?: string;
+}
+
+interface CardProps {
+  route: RouteResponse["data"];
 }
 
 const getRoutePoints = (route: { id?: string; type?: string; attributes?: RouteModel }) => {
@@ -19,8 +29,77 @@ const getRoutePoints = (route: { id?: string; type?: string; attributes?: RouteM
   return [start, ...otherPoints];
 };
 
+const RouteCard: FC<CardProps> = ({ route }) => {
+  const intl = useIntl();
+
+  if (!route) {
+    return null;
+  }
+
+  const routePoints = getRoutePoints(route);
+  const line = turf.lineString(routePoints);
+  const start = routePoints[0];
+  const end = routePoints[routePoints.length - 1];
+  const length = turf.length(line, { units: "kilometers" }).toFixed(1);
+
+  return (
+    <MapCard title={route.attributes?.name || ""} position="bottom-right">
+      <ul className="c-card__text c-card__list">
+        <li>
+          <FormattedMessage
+            id="route.start"
+            values={{
+              value: `${start[0]?.toFixed(4)}, ${start[1].toFixed(4)}`
+            }}
+          />
+        </li>
+        <li>
+          <FormattedMessage
+            id="route.end"
+            values={{
+              value: `${end[0].toFixed(4)}, ${end[1].toFixed(4)}`
+            }}
+          />
+        </li>
+        <li>
+          <FormattedMessage id="route.distance" values={{ value: length }} />
+        </li>
+        <li>
+          <FormattedMessage
+            id="route.date"
+            values={{
+              value: intl.formatDate(route.attributes?.startDate, { month: "short", day: "2-digit", year: "numeric" })
+            }}
+          />
+        </li>
+        <li>
+          <FormattedMessage
+            id="route.difficulty"
+            values={{ value: intl.formatMessage({ id: `route.difficulty.${route.attributes?.difficulty}` }) }}
+          />
+        </li>
+        <li>
+          <FormattedMessage
+            id="route.timeTaken"
+            values={{
+              value: capitalizeFirstLetter(
+                moment.duration(moment(route.attributes?.endDate).diff(moment(route.attributes?.startDate))).humanize()
+              )
+            }}
+          />
+        </li>
+        <li>
+          <FormattedMessage id="route.monitor" values={{ value: route.attributes?.createdBy }} />
+        </li>
+      </ul>
+    </MapCard>
+  );
+};
+
 const AreaRoutesSource: FC<IProps> = props => {
+  const [selectedRoute, setSelectedRoute] = useState<RouteResponse["data"] | null>(null);
   const { httpAuthHeader } = useAccessToken();
+  const { current: mapRef } = useMap();
   const { data: teamRoutes } = useGetV3GfwRoutesTeams({
     headers: httpAuthHeader
   });
@@ -39,7 +118,10 @@ const AreaRoutesSource: FC<IProps> = props => {
   const routesMapped = useMemo(
     () =>
       routes.map(route => {
-        return turf.lineString(getRoutePoints(route));
+        const geojson = turf.lineString(getRoutePoints(route));
+        // @ts-ignore
+        geojson.properties.route = route;
+        return geojson;
       }),
     [routes]
   );
@@ -60,7 +142,9 @@ const AreaRoutesSource: FC<IProps> = props => {
           // @ts-ignore
           newPoint.properties.isEndPoint = indexOfPoint === 0 || indexOfPoint === locations.length - 1;
           // @ts-ignore TODO - change based on has the route been selected - will change the style.
-          newPoint.properties.isSelected = true;
+          newPoint.properties.isSelected = selectedRoute?.id === route.id;
+          // @ts-ignore
+          newPoint.properties.route = route;
 
           return newPoint;
         });
@@ -68,8 +152,23 @@ const AreaRoutesSource: FC<IProps> = props => {
         points.features = newFeatures;
         return points;
       }),
-    [routes]
+    [routes, selectedRoute?.id]
   );
+
+  useEffect(() => {
+    const handleMapClick = (e: MapMouseEvent & EventData) => {
+      const route = JSON.parse(e.features[0].properties.route);
+      setSelectedRoute(route);
+    };
+
+    routesMapped.forEach((routes, index) => mapRef?.on("click", `route-lines-${index}`, handleMapClick));
+    pointsMapped.forEach((points, index) => mapRef?.on("click", `route-points-${index}`, handleMapClick));
+
+    return () => {
+      routesMapped.forEach((routes, index) => mapRef?.off("click", `route-lines-${index}`, handleMapClick));
+      pointsMapped.forEach((points, index) => mapRef?.off("click", `route-points-${index}`, handleMapClick));
+    };
+  }, [mapRef, pointsMapped, routesMapped]);
 
   return (
     <>
@@ -77,7 +176,7 @@ const AreaRoutesSource: FC<IProps> = props => {
         routesMapped.map((route, index) => (
           <Source key={index} type="geojson" data={route}>
             {/* @ts-ignore TS typing error with Layer */}
-            <Layer {...lineStyle} />
+            <Layer {...lineStyle} id={`route-lines-${index}`} />
           </Source>
         ))}
 
@@ -85,9 +184,10 @@ const AreaRoutesSource: FC<IProps> = props => {
         pointsMapped.map((point, index) => (
           <Source type="geojson" data={point} key={index}>
             {/* @ts-ignore TS typing error with Layer */}
-            <Layer {...linePointStyle} />
+            <Layer {...linePointStyle} id={`route-points-${index}`} />
           </Source>
         ))}
+      {selectedRoute && <RouteCard route={selectedRoute} />}
     </>
   );
 };
