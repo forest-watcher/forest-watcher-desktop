3c2343158056642af20fee1324ca2295f596c968
owenjs <owen.evans115@gmail.com>
2023-01-05 14:11:29 +0000

    added a thumbnail fallback for area cards

--- src/components/area-card/AreaCard.tsx
+++ src/components/area-card/AreaCard.tsx
@@ -1,9 +1,10 @@
-import { FC, HTMLAttributes } from "react";
+import { FC, HTMLAttributes, ReactEventHandler } from "react";
 import { FormattedMessage } from "react-intl";
 import Card from "components/ui/Card/Card";
 import EditIcon from "assets/images/icons/Edit.svg";
 import { TAreasResponse } from "services/area";
 import classNames from "classnames";
+import DefaultAreaThumbnail from "assets/images/DefaultAreaThumbnail.svg";
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
   area: TAreasResponse;
@@ -12,9 +13,19 @@ interface IProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 const AreaCard: FC<IProps> = ({ className, area, subtitleKey, subtitleValue }) => {
+  const handleThumbnailLoadError: ReactEventHandler<HTMLImageElement> = e => {
+    e.currentTarget.src = DefaultAreaThumbnail;
+  };
+
   return (
     <Card size="large" className={classNames("c-area-card", className)}>
-      <Card.Image alt="" src={area.attributes.image} loading="lazy" className="c-area-card__image" />
+      <Card.Image
+        alt=""
+        src={area.attributes.image}
+        loading="lazy"
+        onError={handleThumbnailLoadError}
+        className="c-area-card__image"
+      />
       <div className="c-card__content-flex">
         <div className="u-text-ellipsis u-flex-1">
           <Card.Title className="u-margin-top-none">{area.attributes.name}</Card.Title>
