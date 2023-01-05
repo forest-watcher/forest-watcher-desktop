e9e655ea31a94b515b20c1b8dc9b4d9f1e5fcf33
owenjs <owen.evans115@gmail.com>
2023-01-05 11:03:36 +0000

    map keys added to map legend

--- src/components/ui/Map/components/MapLegendControl.tsx
+++ src/components/ui/Map/components/MapLegendControl.tsx
@@ -1,12 +1,42 @@
 import Icon from "components/extensive/Icon";
 import { FC } from "react";
 import { Popover } from "@headlessui/react";
+import { FormattedMessage } from "react-intl";
 
 export interface IProps {}
 
-const MapLegendControl: FC<IProps> = props => {
-  // const {} = props;
+const MAP_KEYS = [
+  {
+    icon: "mapKeyIcons/Alerts",
+    label: "maps.legend.key.alerts"
+  },
+  {
+    icon: "mapKeyIcons/MyReports",
+    label: "maps.legend.key.my.reports"
+  },
+  {
+    icon: "mapKeyIcons/ImportedReports",
+    label: "maps.legend.key.imported.reports"
+  },
+  {
+    icon: "mapKeyIcons/VIIRSAlerts",
+    label: "maps.legend.key.VIIRS.alerts"
+  },
+  {
+    icon: "mapKeyIcons/VIIRSReports",
+    label: "maps.legend.key.VIIRS.reports"
+  },
+  {
+    icon: "mapKeyIcons/AssignmentsCreated",
+    label: "maps.legend.key.assignments.created"
+  },
+  {
+    icon: "mapKeyIcons/AssignmentsAssigned",
+    label: "maps.legend.key.assignments.assigned"
+  }
+];
 
+const MapLegendControl: FC<IProps> = () => {
   return (
     <Popover className="c-map__search-controls">
       <Popover.Button className="rounded-md border-2 border-solid border-neutral-400 bg-neutral-300 inline-flex items-center text-neutral-700 text-lg h-[41px] font-fira hover:bg-neutral-400">
@@ -15,7 +45,7 @@ const MapLegendControl: FC<IProps> = props => {
       </Popover.Button>
 
       {/*Key Legend*/}
-      <Popover.Panel className="c-map-legend rounded-md bg-neutral-300 border-2 w-[350px] h-[330px] absolute top-0 right-0 border-neutral-400 border-solid">
+      <Popover.Panel className="c-map-legend flex flex-col rounded-md bg-neutral-300 border-2 w-[350px] h-[330px] absolute top-0 right-0 border-neutral-400 border-solid">
         {({ close }) => (
           <>
             {/*Key Header*/}
@@ -32,8 +62,17 @@ const MapLegendControl: FC<IProps> = props => {
             </div>
 
             {/*Key List*/}
-            <div className="p-5 pr-2" style={{ color: "red" }}>
-              <div className="overflow-auto">Hello OE</div>
+            <div className="p-5 pr-2 overflow-auto">
+              <div className="flex flex-col gap-y-4">
+                {MAP_KEYS.map(({ icon, label }) => (
+                  <div key={label} className="flex items-center">
+                    <Icon className="mr-[0.688rem]" name={icon} />
+                    <span className="text-neutral-600 font-fira text-base">
+                      <FormattedMessage id={label} />
+                    </span>
+                  </div>
+                ))}
+              </div>
             </div>
           </>
         )}
