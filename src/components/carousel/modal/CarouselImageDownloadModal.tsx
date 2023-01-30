b73e2349d4ad0941c755baef012398648854015b
Kevin Borrill <kevin.borrill@3sidedcube.com>
2023-01-30 17:18:44 +0000

    Adjust carousel image padding and remove slider if only one image

--- src/components/carousel/modal/CarouselImageDownloadModal.tsx
+++ src/components/carousel/modal/CarouselImageDownloadModal.tsx
@@ -1,4 +1,5 @@
 import Modal from "components/ui/Modal/Modal";
+import { useIntl } from "react-intl";
 
 type CarouselImageDownloadModalProps = {
   onClose: () => void;
@@ -6,6 +7,7 @@ type CarouselImageDownloadModalProps = {
 };
 
 const CarouselImageDownloadModal = ({ onClose, imageToDownload }: CarouselImageDownloadModalProps) => {
+  const intl = useIntl();
   const handleDownload = () => {
     window.open(imageToDownload, "_blank");
     return onClose();
@@ -15,15 +17,15 @@ const CarouselImageDownloadModal = ({ onClose, imageToDownload }: CarouselImageD
     <Modal
       isOpen
       onClose={onClose}
-      title={"Images"}
+      title={intl.formatMessage({ id: "questionTypes.blob" })}
       className="c-modal-form"
       actions={[
         { name: "common.download", onClick: handleDownload },
         { name: "common.cancel", variant: "secondary", onClick: onClose }
       ]}
     >
-      <div className="border-2 border-solid border-neutral-400 rounded-lg flex items-center justify-center h-full py-4">
-        <img src={imageToDownload} alt="" className="h-full" />
+      <div className="border-2 border-solid border-neutral-400 rounded-lg flex items-center justify-center h-full">
+        <img src={imageToDownload} alt="" className="h-full w-full object-contain" />
       </div>
     </Modal>
   );
