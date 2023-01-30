98a6c0bbe7f1d5788e6160733b1c4ba9765cb71e
Kevin Borrill <kevin.borrill@3sidedcube.com>
2023-01-30 14:47:04 +0000

    Update List component to be able to auto scroll when new element added

--- src/components/extensive/List.tsx
+++ src/components/extensive/List.tsx
@@ -1,14 +1,36 @@
+import { useCallback, useState } from "react";
+
 type ListProps<T> = {
   items: T[];
   render: (item: T, index: number) => JSX.Element | null;
   loading?: boolean;
   className?: string;
   itemClassName?: string;
+  autoScrollToBottom?: boolean;
 };
 
-const List = <T extends Record<any, any>>({ items = [], render, className, itemClassName }: ListProps<T>) => {
+const List = <T extends Record<any, any>>({
+  items = [],
+  render,
+  className,
+  itemClassName,
+  autoScrollToBottom = false
+}: ListProps<T>) => {
+  const [lengthOfRenderedItems, setLengthOfRenderedItems] = useState(items.length);
+
+  const handleRefChange = useCallback(
+    (e: HTMLUListElement) => {
+      if (e?.children.length > lengthOfRenderedItems && autoScrollToBottom) {
+        window.scrollTo({ top: e.offsetHeight });
+      }
+
+      setLengthOfRenderedItems(items.length);
+    },
+    [autoScrollToBottom, items.length, lengthOfRenderedItems]
+  );
+
   return (
-    <ul className={className}>
+    <ul ref={handleRefChange} className={className}>
       {items?.map?.((item, i) => (
         <li key={i} className={itemClassName}>
           {render(item, i)}
