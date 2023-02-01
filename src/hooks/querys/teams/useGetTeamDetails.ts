2b2c0a98290df73e3c0b848a751f2842c3669a88
owenjs <owen.evans115@gmail.com>
2023-02-01 16:40:07 +0000

    lowered stale time for team listing and detail views

--- src/hooks/querys/teams/useGetTeamDetails.ts
+++ src/hooks/querys/teams/useGetTeamDetails.ts
@@ -16,7 +16,7 @@ const useGetTeamDetails = (teamId?: string) => {
     },
     {
       enabled: !!teamId,
-      staleTime: 1000 * 60 * 5 // 5 minutes
+      staleTime: 1000 * 60 // 1 minutes
     }
   );
 
--- src/hooks/querys/teams/useGetUserTeams.ts
+++ src/hooks/querys/teams/useGetUserTeams.ts
@@ -18,7 +18,7 @@ const useGetUserTeams = () => {
     },
     {
       enabled: !!userId,
-      staleTime: 1000 * 60 * 5 // 5 minutes
+      staleTime: 1000 * 60 // 1 minutes
     }
   );
 
