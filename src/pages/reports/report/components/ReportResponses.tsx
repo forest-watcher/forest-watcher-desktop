6ee10db7ac67911299c0110568939c51a9e2f64c
owenjs <owen.evans115@gmail.com>
2023-02-17 15:34:36 +0000

    Question prefixes now translated

--- src/pages/reports/report/components/ReportResponses.tsx
+++ src/pages/reports/report/components/ReportResponses.tsx
@@ -4,7 +4,7 @@ import Button from "components/ui/Button/Button";
 import { AnswerResponse, ReportsQuestion } from "generated/forms/formsSchemas";
 import { useAppSelector } from "hooks/useRedux";
 import { useMemo, useState } from "react";
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
 import ReportExportImagesModal from "./modal/ReportExportImagesModal";
 import ReportResponse, { ReportResponseProps } from "./ReportResponse";
 
@@ -17,71 +17,84 @@ type ReportResponsesProps = {
 const ReportResponses = ({ defaultLanguage, questions, responses }: ReportResponsesProps) => {
   const [showImagesModal, setShowImagesModal] = useState<boolean>(false);
   const { locale } = useAppSelector(state => state.app);
+  const intl = useIntl();
 
   const data = useMemo(() => {
-    return questions?.reduce<ReportResponseProps[]>((combinedResponses, currentQuestionDetails) => {
-      const copyCombinedResponses = [...combinedResponses];
+    return questions?.reduce<ReportResponseProps[]>(
+      (combinedResponses, currentQuestionDetails, currentQuestionIndex) => {
+        const copyCombinedResponses = [...combinedResponses];
 
-      const response = responses.find(r => r.name === currentQuestionDetails.name);
-      if (!response) return copyCombinedResponses;
+        const response = responses.find(r => r.name === currentQuestionDetails.name);
+        if (!response) return copyCombinedResponses;
 
-      const generateCombineResponse = (name: string, label: string, type: string, value?: string) => {
-        const formattedQuestion = name
-          .split("-")
-          .map(s => s[0].toUpperCase() + s.slice(1))
-          .join(" ");
-
-        return {
-          question: `${formattedQuestion}: ${label}`,
-          response: value ?? null,
+        const generateCombineResponse = ({
+          label,
           type,
-          childQuestions: [] as AnswerResponse[]
+          value,
+          childQuestion = false
+        }: {
+          label: string;
+          type: string;
+          value?: string;
+          childQuestion?: boolean;
+        }) => {
+          const formattedQuestion = intl.formatMessage(
+            { id: childQuestion ? "reports.preview.childQuestion.id" : "reports.preview.question.id" },
+            { index: currentQuestionIndex + 1 }
+          );
+
+          return {
+            question: `${formattedQuestion} ${label}`,
+            response: value ?? null,
+            type,
+            childQuestions: [] as AnswerResponse[]
+          };
         };
-      };
 
-      const combineResponse = generateCombineResponse(
-        currentQuestionDetails.name,
-        // @ts-ignore
-        currentQuestionDetails.label[locale] ?? currentQuestionDetails.label[defaultLanguage],
-        currentQuestionDetails.type,
-        response.value
-      );
+        const combineResponse = generateCombineResponse({
+          // @ts-ignore
+          label: currentQuestionDetails.label[locale] ?? currentQuestionDetails.label[defaultLanguage],
+          type: currentQuestionDetails.type,
+          value: response.value
+        });
 
-      // When the question type is "audio"
-      // Each child question contains the audio file information
-      // Add this to the combineResponse object for later
-      // @see src/pages/reports/report/components/ReportResponse.tsx
-      if (currentQuestionDetails.type === "audio") {
-        combineResponse.childQuestions = responses.filter(response =>
-          currentQuestionDetails.childQuestions?.find(question => question.name === response.name)
-        );
-      }
-
-      copyCombinedResponses.push(combineResponse);
-
-      // When the question type isn't "audio"
-      // Each child question is considered a separate response
-      // Generate a Combined Response for each child
-      if (currentQuestionDetails.type !== "audio" && currentQuestionDetails.childQuestions?.length) {
-        for (let i = 0; i < currentQuestionDetails.childQuestions.length; i++) {
-          const childQuestionDetails = currentQuestionDetails.childQuestions[i];
-          const childQuestionResponse = responses.find(r => r.name === childQuestionDetails.name);
-
-          copyCombinedResponses.push(
-            generateCombineResponse(
-              childQuestionDetails.name,
-              // @ts-ignore
-              childQuestionDetails.label[locale] ?? childQuestionDetails.label[defaultLanguage],
-              childQuestionDetails.type,
-              childQuestionResponse?.value
-            )
+        // When the question type is "audio"
+        // Each child question contains the audio file information
+        // Add this to the combineResponse object for later
+        // @see src/pages/reports/report/components/ReportResponse.tsx
+        if (currentQuestionDetails.type === "audio") {
+          combineResponse.childQuestions = responses.filter(response =>
+            currentQuestionDetails.childQuestions?.find(question => question.name === response.name)
           );
         }
-      }
 
-      return copyCombinedResponses;
-    }, []);
-  }, [questions, responses, locale, defaultLanguage]);
+        copyCombinedResponses.push(combineResponse);
+
+        // When the question type isn't "audio"
+        // Each child question is considered a separate response
+        // Generate a Combined Response for each child
+        if (currentQuestionDetails.type !== "audio" && currentQuestionDetails.childQuestions?.length) {
+          for (let i = 0; i < currentQuestionDetails.childQuestions.length; i++) {
+            const childQuestionDetails = currentQuestionDetails.childQuestions[i];
+            const childQuestionResponse = responses.find(r => r.name === childQuestionDetails.name);
+
+            copyCombinedResponses.push(
+              generateCombineResponse({
+                // @ts-ignore
+                label: childQuestionDetails.label[locale] ?? childQuestionDetails.label[defaultLanguage],
+                type: childQuestionDetails.type,
+                value: childQuestionResponse?.value,
+                childQuestion: true
+              })
+            );
+          }
+        }
+
+        return copyCombinedResponses;
+      },
+      []
+    );
+  }, [questions, responses, locale, defaultLanguage, intl]);
 
   const hasImages = useMemo(() => {
     const found = data.find(item => item.type === "blob");
