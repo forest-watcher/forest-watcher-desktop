54c97b8a48ac31f29836b672d56ab16ab210576b
owenjs <owen.evans115@gmail.com>
2023-02-28 15:23:58 +0000

    what new content updated and translations added

--- src/components/onboarding/monitoring/AreasOnboarding.tsx
+++ src/components/onboarding/monitoring/AreasOnboarding.tsx
@@ -1,35 +1,40 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useLocalStorage } from "hooks/useLocalStorage";
+import { useIntl } from "react-intl";
 import OnboardingModal, { OnboardingStep } from "../OnboardingModal";
 import OptionalWrapper from "components/extensive/OptionalWrapper";
 import AddAreasImage from "assets/images/onboarding/add-areas.png";
 import MonitorAreasImage from "assets/images/onboarding/monitor-areas.png";
 import ReviewTeamsImage from "assets/images/onboarding/review-teams.png";
 
-const steps: OnboardingStep[] = [
-  {
-    heading: "New in this version",
-    title: "Add Areas",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique enim ac eros pellentesque commodo. Nullam sit amet rutrum risus. Donec mollis lectus ullamcorper pretium faucibus. Cras velit lorem, mollis ac placerat vel, fermentum eleifend tellus. ",
-    imageUrl: AddAreasImage
-  },
-  {
-    heading: "New in this version",
-    title: "Monitor Areas",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique enim ac eros pellentesque commodo. Nullam sit amet rutrum risus. Donec mollis lectus ullamcorper pretium faucibus. Cras velit lorem, mollis ac placerat vel, fermentum eleifend tellus. ",
-    imageUrl: MonitorAreasImage
-  },
-  {
-    heading: "New in this version",
-    title: "Review Teams",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tristique enim ac eros pellentesque commodo. Nullam sit amet rutrum risus. Donec mollis lectus ullamcorper pretium faucibus. Cras velit lorem, mollis ac placerat vel, fermentum eleifend tellus. ",
-    imageUrl: ReviewTeamsImage
-  }
-];
-
 const AreasOnboarding = () => {
   const { get, set } = useLocalStorage();
   const [show, setShow] = useState<boolean>(false);
+  const intl = useIntl();
+
+  const steps = useMemo<OnboardingStep[]>(
+    () => [
+      {
+        heading: intl.formatMessage({ id: "what.new.1.heading" }),
+        title: intl.formatMessage({ id: "what.new.1.title" }),
+        text: intl.formatMessage({ id: "what.new.1.text" }),
+        imageUrl: AddAreasImage
+      },
+      {
+        heading: intl.formatMessage({ id: "what.new.2.heading" }),
+        title: intl.formatMessage({ id: "what.new.2.title" }),
+        text: intl.formatMessage({ id: "what.new.2.text" }),
+        imageUrl: MonitorAreasImage
+      },
+      {
+        heading: intl.formatMessage({ id: "what.new.3.heading" }),
+        title: intl.formatMessage({ id: "what.new.3.title" }),
+        text: intl.formatMessage({ id: "what.new.3.text" }),
+        imageUrl: ReviewTeamsImage
+      }
+    ],
+    [intl]
+  );
 
   /**
    * Handles check onboarded in LS and conditionally show Monitoring onboarding.
